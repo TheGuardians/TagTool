f045708d0c8b752116df1187e3e5f428abd30e48
Camden Smallwood <camden.smallwood@gmail.com>
2017-11-30 21:49:43 -0500

    Some cleanup, update BlamCore version

--- TagTool/Commands/Tags/SaveTagNamesCommand.cs
+++ TagTool/Commands/Tags/SaveTagNamesCommand.cs
@@ -1,6 +1,7 @@
 ﻿using BlamCore.Cache;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace TagTool.Commands.Tags
 {
@@ -26,16 +27,7 @@ namespace TagTool.Commands.Tags
             if (args.Count > 1)
                 return false;
 
-            var csvFile = (args.Count == 1) ?
-                new FileInfo(args[0]) :
-                new FileInfo(Path.Combine(CacheContext.Directory.FullName, "tag_list.csv"));
-
-            if (!csvFile.Directory.Exists)
-                csvFile.Directory.Create();
-
-            using (var csvWriter = new StreamWriter(csvFile.Create()))
-                foreach (var entry in CacheContext.TagNames)
-                    csvWriter.WriteLine($"0x{entry.Key:X8},{entry.Value}");
+            CacheContext.SaveTagNames(args.Count == 1 ? args[0] : null);
 
             return true;
         }
