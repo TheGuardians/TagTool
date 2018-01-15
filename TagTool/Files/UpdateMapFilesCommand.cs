6383abb2fb5ea75a504a01c98c6f71a2bd4d377d
Camden Smallwood <camden.smallwood@gmail.com>
2018-01-15 16:00:25 -0500

    Simplify and fix 'UpdateMapFiles' command

--- TagTool/Files/UpdateMapFilesCommand.cs
+++ TagTool/Files/UpdateMapFilesCommand.cs
@@ -1,12 +1,9 @@
-﻿using System;
+﻿using BlamCore.Cache;
+using BlamCore.Commands;
+using BlamCore.Common;
+using System;
 using System.Collections.Generic;
 using System.IO;
-using BlamCore.Cache;
-using BlamCore.Common;
-using BlamCore.TagDefinitions;
-using BlamCore.Serialization;
-using BlamCore.IO;
-using BlamCore.Commands;
 
 namespace TagTool.Files
 {
@@ -32,96 +29,51 @@ namespace TagTool.Files
             if (args.Count != 0)
                 return false;
 
-            var mapIndices = new Dictionary<int, int>();
-            var mapScenarios = new Dictionary<int, Scenario_mapId>();
+            var mapScenarioIndices = new Dictionary<int, int>();
 
-            using (var cacheStream = CacheContext.OpenTagCacheRead())
+            using (var stream = CacheContext.OpenTagCacheRead())
+            using (var reader = new BinaryReader(stream))
             {
-                foreach (var scnrTag in CacheContext.TagCache.Index.FindAllInGroup("scnr"))
+                foreach (var tag in CacheContext.TagCache.Index.FindAllInGroup("scnr"))
                 {
-                    var tagContext = new TagSerializationContext(cacheStream, CacheContext, scnrTag);
-                    var scnr = CacheContext.Deserializer.Deserialize<Scenario_mapId>(tagContext);
-                    mapIndices[scnr.MapId] = scnrTag.Index;
-                    mapScenarios[scnr.MapId] = scnr;
+                    if (tag.HeaderOffset == -1)
+                        continue;
+
+                    reader.BaseStream.Position = tag.HeaderOffset + tag.DefinitionOffset + 0x8;
+                    mapScenarioIndices[reader.ReadInt32()] = tag.Index;
                 }
             }
 
             foreach (var mapFile in CacheContext.Directory.GetFiles("*.map"))
             {
-                try
+                using (var stream = mapFile.Open(FileMode.Open, FileAccess.ReadWrite))
+                using (var reader = new BinaryReader(stream))
+                using (var writer = new BinaryWriter(stream))
                 {
-                    using (var stream = mapFile.Open(FileMode.Open, FileAccess.ReadWrite))
-                    using (var reader = new EndianReader(stream))
-                    using (var writer = new EndianWriter(stream))
+                    if (reader.ReadInt32() != new Tag("head").Value)
                     {
-                        if (reader.ReadInt32() != new Tag("head").Value)
-                        {
-                            Console.Error.WriteLine("Invalid map file");
-                            return true;
-                        }
-
-                        reader.BaseStream.Position = 0x2DEC;
-                        var mapId = reader.ReadInt32();
-
-                        if (mapIndices.ContainsKey(mapId))
-                        {
-                            var mapIndex = mapIndices[mapId];
-
-                            writer.BaseStream.Position = 0x2DF0;
-                            writer.Write(mapIndex);
-
-                            Console.WriteLine($"Scenario tag index for {mapFile.Name}: {mapIndex:X8}");
-
-                            if (mapScenarios.ContainsKey(mapId) &&
-                                mapScenarios[mapId].MapType == MapTypeValue.Multiplayer)
-                            {
-                                var dataContext = new DataSerializationContext(reader, writer);
-
-                                stream.Seek(0xBD80, SeekOrigin.Begin);
-                                var mapVariant = CacheContext.Deserializer.Deserialize<MapVariant>(dataContext);
-
-                                foreach (var entry in mapVariant.BudgetEntries)
-                                    if ((entry.TagIndex != -1) &&
-                                        (CacheContext.GetTag(entry.TagIndex) == null))
-                                        entry.TagIndex = -1;
-
-                                stream.Seek(0xBD80, SeekOrigin.Begin);
-                                CacheContext.Serializer.Serialize(dataContext, mapVariant);
-                            }
-                            else
-                            {
-                                stream.Seek(0x3390, SeekOrigin.Begin);
-                            }
-
-                            stream.SetLength(stream.Position);
-                        }
+                        Console.Error.WriteLine("Invalid map file");
+                        return true;
                     }
-                }
-                catch (IOException)
-                {
-                    Console.Error.WriteLine("Unable to open the map file for reading.");
+
+                    reader.BaseStream.Position = 0x2DEC;
+                    var mapId = reader.ReadInt32();
+
+                    if (!mapScenarioIndices.ContainsKey(mapId))
+                        continue;
+
+                    var mapIndex = mapScenarioIndices[mapId];
+
+                    writer.BaseStream.Position = 0x2DF0;
+                    writer.Write(mapIndex);
+
+                    Console.WriteLine($"Scenario tag index for {mapFile.Name}: {mapIndex:X8}");
                 }
             }
 
             Console.WriteLine("Done!");
 
             return true;
-        }
-                
-        [TagStructure(Name = "scenario", Tag = "scnr", MinVersion = CacheVersion.Halo3Retail)]
-        private class Scenario_mapId
-        {
-            [TagField(MinVersion = CacheVersion.Halo3Retail, MaxVersion = CacheVersion.Halo3Retail)]
-            public byte MapTypePadding = 0;
-
-            public MapTypeValue MapType = MapTypeValue.Multiplayer;
-
-            [TagField(MinVersion = CacheVersion.Halo3ODST)]
-            public MapSubTypeValue MapSubType = MapSubTypeValue.None;
-
-            public ScenarioFlags Flags = ScenarioFlags.None;
-            public int CampaignId = 1;
-            public int MapId = 0;
         }
     }
 }
