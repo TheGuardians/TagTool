16467692c0707a03fd893bc64d5b8198e00bf6e2
Haydn Trigg <me@haydntrigg.com>
2018-03-01 07:33:32 +1030

    Helpers

---
+++ TagTool/ShaderGenerator/shader_code/helpers.hlsl
@@ -0,0 +1,16 @@
+﻿#ifndef __HELPERS
+#define __HELPERS
+
+float3 NormalMapSample(sampler map, float2 texture_coordinate)
+{
+    float4 normal_map = tex2D(map, texture_coordinate);
+
+    float2 normal_xy = normal_map.xy * 2.00787401 + -1.00787401;
+    float2 normal_xy2 = normal_xy * normal_xy;
+    float remainder = 1.0 - saturate(normal_xy2.x + normal_xy2.y);
+    float normal_z = sqrt(remainder);
+
+    return float3(normal_xy, normal_z);
+}
+
+#endif
