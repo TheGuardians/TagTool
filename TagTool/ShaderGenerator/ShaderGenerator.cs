7743cbbc1e80b908b68d7be5e117ef5f88194df7
Haydn Trigg <me@haydntrigg.com>
2018-02-28 17:24:36 +1030

    Change Namespace of HaloShaderGenerator to TagTool.ShaderGenerator

--- TagTool/ShaderGenerator/ShaderGenerator.cs
+++ TagTool/ShaderGenerator/ShaderGenerator.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HaloShaderGenerator
+namespace TagTool.ShaderGenerator
 {
     public partial class ShaderGenerator
     {
