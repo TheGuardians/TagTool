e8c3863f2c1c76c03e9649a3ca78251fcdd8040f
jabukufo <songjesterserver@gmail.com>
2018-04-24 06:24:36 -0500

    Fixed missing 'using TagTool.Direct3D.Enums;' compiler error.

--- TagTool/Direct3D/Functions/Assemble.cs
+++ TagTool/Direct3D/Functions/Assemble.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using TagTool.Direct3D.Enums;
 using TagTool.Direct3D.Interfaces;
 
 namespace TagTool.Direct3D.Functions
