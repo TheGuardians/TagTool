c82193b934d951ce6edc2a872e38363a951e6bac
Camden Smallwood <camden.smallwood@gmail.com>
2018-08-16 14:08:46 -0400

    Refactor 'styl' tag conversion

--- TagTool/Commands/Porting/PortTagCommand.AI.cs
+++ TagTool/Commands/Porting/PortTagCommand.AI.cs
@@ -11,57 +11,26 @@ namespace TagTool.Commands.Porting
         {
             if (BlamCache.Version == CacheVersion.Halo3Retail)
             {
-                //Note : Might have to hardcode some of the flags like fight_positining, kungfu, inspect and engineers from tag names. For now they are always off.
+                //
+                // Collect halo 3 behavior states
+                //
 
-                /*
-                 * squad : 0x4000 B1
-                 * fight positioning : 0x10000000 B1
-                 * kungfu : 0x80000 B3
-                 * inspect : 0x80 B5
-                 * Engineer start : 0xE0000000 B5
-                 * Engineer end : 0x1F B6
-                 * 
-                 */
-                //Style control is the same
+                var behaviors = new Dictionary<string, bool>();
 
-                var H3B1 = style.Behaviors1;
-                var H3B2 = style.Behaviors2;
-                var H3B3 = style.Behaviors3;
-                var H3B4 = style.Behaviors4;
-                var H3B5 = style.Behaviors5;
-                var H3B6 = style.Behaviors6;
-                var H3B7 = style.Behaviors7;
+                for (var i = 0; i < style.Behaviors.Length; i++)
+                    for (var j = 0; j < 32 && ((i * 32) + j) < style.BehaviorList.Count; j++)
+                        behaviors[style.BehaviorList[(i * 32) + j].BehaviorName] = ((style.Behaviors[i] & (1 << j)) != 0);
 
-                //                 First part      shift by one the rest        add 3 remaining of behavior 1
+                //
+                // Clear halo 3 behaviors
+                //
 
-                style.Behaviors1 = (H3B1 & 0x3FFF) + ((H3B1 & 0x7FFC000) << 1) + ((H3B1 & 0x38000000) << 2);
+                for (var i = 0; i < style.Behaviors.Length; i++)
+                    style.Behaviors[i] = 0;
 
-                //                  Reuse 2 flags of H3B1       Use all of H3B2 except 2 last
-
-                style.Behaviors2 = ((H3B1 & 0xC0000000) >> 30) + ((H3B2 & 0x3FFFFFFF) << 2);
-
-                //                  Reuse 2 flags of H3B2        Some flags of H3B3     rest of flags
-
-                style.Behaviors3 = ((H3B2 & 0xC0000000) >> 30) + ((H3B3 & 0x1FFFF) << 2) + ((H3B3 & 0x1FFE0000) << 3);
-
-                //                  Reuse 3 flags of H3B3         Some flags of H3B4
-
-                style.Behaviors4 = ((H3B3 & 0xE0000000) >> 29) + ((H3B4 & 0x1FFFFFFF) << 3);
-
-                //                  Reuse 3 flags of H3B4         Some flags of H3B5     more flags of H3B5         
-
-                style.Behaviors5 = ((H3B4 & 0xE0000000) >> 29) + ((H3B5 & 0x7F) << 3) + ((H3B5 & 0x1FFFF80) << 4);
-
-                //                  rest of H3B5            20 first flags of H3B6
-
-                style.Behaviors6 = ((H3B5 & 0xFE000000) >> 20) + ((H3B6 & 0xFFFFF) << 12);
-
-                //              12 last flags of H3B6            5 flags of H3B7
-
-                style.Behaviors7 = ((H3B6 & 0xFFF00000) >> 20) + ((H3B7 & 0xFFFFF) << 12);
-
-
-                //Add the new behaviors to the list at the end of the tag:
+                //
+                // Insert odst styles into the halo 3 behavior list
+                //
 
                 style.BehaviorList.Insert(14, new Style.BehaviorListBlock { BehaviorName = "squad_patrol_behavior" });
                 style.BehaviorList.Insert(28, new Style.BehaviorListBlock { BehaviorName = "fight_positioning" });
@@ -78,6 +47,23 @@ namespace TagTool.Commands.Porting
                 style.BehaviorList.Insert(162, new Style.BehaviorListBlock { BehaviorName = "engineer_explode" });
                 style.BehaviorList.Insert(163, new Style.BehaviorListBlock { BehaviorName = "engineer_broken_detonation" });
                 style.BehaviorList.Insert(164, new Style.BehaviorListBlock { BehaviorName = "boost_allies" });
+
+                //
+                // Set odst behaviors
+                //
+
+                for (var i = 0; i < style.Behaviors.Length; i++)
+                {
+                    for (var j = 0; j < 32 && ((i * 32) + j) < style.BehaviorList.Count; j++)
+                    {
+                        var behavior = style.BehaviorList[(i * 32) + j].BehaviorName;
+
+                        if (!behaviors.ContainsKey(behavior) || !behaviors[behavior])
+                            continue;
+
+                        style.Behaviors[i] |= 1 << j;
+                    }
+                }
             }
 
             return style;
--- TagTool/Tags/Definitions/Style.cs
+++ TagTool/Tags/Definitions/Style.cs
@@ -1,23 +1,26 @@
 using TagTool.Cache;
 using TagTool.Serialization;
 using System.Collections.Generic;
+using System;
 
 namespace TagTool.Tags.Definitions
 {
     [TagStructure(Name = "style", Tag = "styl", Size = 0x5C, MinVersion = CacheVersion.Halo3Retail)]
     public class Style
     {
-        [TagField(Length = 32)] public string Name;
+        [TagField(Length = 32)]
+        public string Name;
+
         public CombatStatusDecayOptionsValue CombatStatusDecayOptions;
-        public short Unknown;
-        public uint StyleControl;
-        public uint Behaviors1;
-        public uint Behaviors2;
-        public uint Behaviors3;
-        public uint Behaviors4;
-        public uint Behaviors5;
-        public uint Behaviors6;
-        public uint Behaviors7;
+
+        [TagField(Padding = true, Length = 2)]
+        public byte[] Unused = new byte[2];
+
+        public StyleControlFlags StyleControl;
+
+        [TagField(Length = 7)]
+        public int[] Behaviors = new int[7];
+
         public List<SpecialMovementBlock> SpecialMovement;
         public List<BehaviorListBlock> BehaviorList;
 
@@ -25,19 +28,40 @@ namespace TagTool.Tags.Definitions
         {
             LatchAtIdle,
             LatchAtAlert,
-            LatchAtCombat,
+            LatchAtCombat
+        }
+
+        [Flags]
+        public enum StyleControlFlags : int
+        {
+            None = 0,
+            NewBehaviorsDefaultToOn = 1 << 0,
+            DoNotForceOnDefaultBehaviors = 1 << 1
         }
 
         [TagStructure(Size = 0x4)]
         public class SpecialMovementBlock
         {
-            public uint SpecialMovement1;
+            public SpecialMovementFlags SpecialMovement1;
+
+            [Flags]
+            public enum SpecialMovementFlags : int
+            {
+                None = 0,
+                Jump = 1 << 0,
+                Climb = 1 << 1,
+                Vault = 1 << 2,
+                Mount = 1 << 3,
+                Hoist = 1 << 4,
+                WallJump = 1 << 5
+            }
         }
 
         [TagStructure(Size = 0x20)]
         public class BehaviorListBlock
         {
-            [TagField(Length = 32)] public string BehaviorName;
+            [TagField(Length = 32)]
+            public string BehaviorName;
         }
     }
 }
