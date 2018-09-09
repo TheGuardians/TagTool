f97a844ed07bae2d2feba150ef454204803ea618
jabukufo <songjesterserver@gmail.com>
2018-09-09 04:21:48 +0000

    Update 'Stopwatch Print' formatting.

--- TagTool/Commands/Common/StopwatchCommand.cs
+++ TagTool/Commands/Common/StopwatchCommand.cs
@@ -33,15 +33,36 @@ namespace TagTool.Commands.Common
 				switch (arg)
 				{
 					case "print":
-                        var miliseconds = _stopWatch.ElapsedMilliseconds;
-                        if (miliseconds <= 1000)
-                        {
-                            Console.WriteLine($"{miliseconds}ms");
-                        }
-                        else
-                        {
-                            Console.WriteLine("{0:0.000}s", (double)_stopWatch.ElapsedMilliseconds / 1000.0f);
-                        }
+                        var r = _stopWatch.ElapsedMilliseconds;
+
+						// days
+						var d = r / 86_400_000;
+						r -= d * 86_400_000;
+						
+						// hours
+						var h = r / 3_600_000;
+						r -= h * 3_600_000;
+
+						// minutes
+						var m = r / 60_000;
+						r -= m * 60_000;
+
+						// seconds
+						var s = r / 1_000;
+						r -= s * 1_000;
+
+						var output = "";
+						output += d > 0 ? $"{d}d " : "";
+						output += h > 0 ? $"{h}h " : "";
+						output += m > 0 ? $"{m}m " : "";
+						output += s > 0 ? $"{s}s " : "";
+						output += r > 0 ? $"{r}ms" : "";
+						output = output != "" ? output : "GOOD GOLLY THAT WAS FAST";
+
+						var startColor = Console.ForegroundColor;
+						Console.ForegroundColor = ConsoleColor.DarkCyan;
+						Console.WriteLine(output);
+						Console.ForegroundColor = startColor;
 						break;
 					case "reset":
 						_stopWatch.Reset();
