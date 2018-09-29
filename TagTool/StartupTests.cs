98bfdc5514d4eb5fc4bb9bcb9588d797de7bb81d
Camden Smallwood <camden.smallwood@gmail.com>
2018-09-28 21:51:31 -0400

    Remove test method (whoops)

--- TagTool/StartupTests.cs
+++ TagTool/StartupTests.cs
@@ -20,35 +20,6 @@ namespace TagTool.Commands
 		}
 
         #region Test Methods
-        private static void TagStructureSizeTest()
-        {
-            var cacheVersions = Enum.GetValues(typeof(CacheVersion)) as CacheVersion[];
-
-            foreach (var type in typeof(TagStructure).Assembly.GetTypes())
-            {
-                foreach (var cacheVersion in cacheVersions)
-                {
-                    var attribute = TagStructure.GetTagStructureAttribute(type, cacheVersion);
-                    Test.Assert(attribute != null,
-                        $"{type.FullName}, {type.Name}, {type}, {type.MemberType}, {type.DeclaringType}, " +
-                        $"{type.ReflectedType}, {type.IsGenericTypeDefinition}, {type.Namespace}, " +
-                        $"{type.AssemblyQualifiedName}, {type.GetMembers().Length}");
-
-                    var structureInfo = TagStructure.GetTagStructureInfo(type, cacheVersion);
-                    var tagFields = TagStructure.GetTagFieldEnumerable(structureInfo);
-
-                    var size = 0U;
-                    foreach (var tagField in tagFields)
-                    {
-                        size += tagField.Size;
-                        Test.Assert(size > 0, "");
-                    }
-
-                    Test.Assert(structureInfo.TotalSize == size, "");
-                }
-            }
-        }
-
         private static void ConfirmTagStructureHasNoFields()
 		{
 			var fields = typeof(TagStructure).GetFields(BindingFlags.Instance | BindingFlags.Public);
