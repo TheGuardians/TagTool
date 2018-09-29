eefb391de62b9dc070af28216830b981c1258b0e
Camden Smallwood <camden.smallwood@gmail.com>
2018-09-28 21:50:45 -0400

    Fix CollisionGeometry size in TagStructure attribute

--- TagTool/StartupTests.cs
+++ TagTool/StartupTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Reflection;
+using TagTool.Cache;
 using TagTool.Tags;
 
 namespace TagTool.Commands
@@ -18,8 +19,37 @@ namespace TagTool.Commands
 			}
 		}
 
-		#region Test Methods
-		private static void ConfirmTagStructureHasNoFields()
+        #region Test Methods
+        private static void TagStructureSizeTest()
+        {
+            var cacheVersions = Enum.GetValues(typeof(CacheVersion)) as CacheVersion[];
+
+            foreach (var type in typeof(TagStructure).Assembly.GetTypes())
+            {
+                foreach (var cacheVersion in cacheVersions)
+                {
+                    var attribute = TagStructure.GetTagStructureAttribute(type, cacheVersion);
+                    Test.Assert(attribute != null,
+                        $"{type.FullName}, {type.Name}, {type}, {type.MemberType}, {type.DeclaringType}, " +
+                        $"{type.ReflectedType}, {type.IsGenericTypeDefinition}, {type.Namespace}, " +
+                        $"{type.AssemblyQualifiedName}, {type.GetMembers().Length}");
+
+                    var structureInfo = TagStructure.GetTagStructureInfo(type, cacheVersion);
+                    var tagFields = TagStructure.GetTagFieldEnumerable(structureInfo);
+
+                    var size = 0U;
+                    foreach (var tagField in tagFields)
+                    {
+                        size += tagField.Size;
+                        Test.Assert(size > 0, "");
+                    }
+
+                    Test.Assert(structureInfo.TotalSize == size, "");
+                }
+            }
+        }
+
+        private static void ConfirmTagStructureHasNoFields()
 		{
 			var fields = typeof(TagStructure).GetFields(BindingFlags.Instance | BindingFlags.Public);
 
