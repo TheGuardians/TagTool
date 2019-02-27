385d0d27620597d1ddbe7de98d6b0f83a9b8b826
Camden Smallwood <camden.smallwood@gmail.com>
2019-02-27 17:52:35 -0500

    More work on script compiler

--- TagTool/Scripting/Compiler/ScriptCompiler.cs
+++ TagTool/Scripting/Compiler/ScriptCompiler.cs
@@ -70,28 +70,109 @@ namespace TagTool.Scripting.Compiler
             }
         }
 
+        private ScriptValueType CompileScriptValueType(IScriptSyntax node)
+        {
+            var result = new ScriptValueType();
+
+            if (!(node is ScriptSymbol symbol) ||
+                !Enum.TryParse(symbol.Value, out result.Halo3ODST))
+            {
+                throw new FormatException(node.ToString());
+            }
+
+            return result;
+        }
+
+        private ScriptType CompileScriptType(IScriptSyntax node)
+        {
+            if (!(node is ScriptSymbol symbol) ||
+                !Enum.TryParse<ScriptType>(symbol.Value, out var result))
+            {
+                throw new FormatException(node.ToString());
+            }
+
+            return result;
+        }
+
         private void CompileGlobal(IScriptSyntax node)
         {
-            switch (node)
+            if (!(node is ScriptGroup group) ||
+                !(group.Head is ScriptSymbol symbol && symbol.Value == "group") ||
+                !(group.Tail is ScriptGroup declGroup))
             {
-                case ScriptGroup group:
-                    throw new NotImplementedException();
-
-                default:
-                    throw new FormatException(node.ToString());
+                throw new FormatException(node.ToString());
             }
+
+            var globalType = CompileScriptValueType(declGroup.Head);
+
+            if (!(declGroup.Tail is ScriptGroup declTailGroup))
+                throw new FormatException(declGroup.Tail.ToString());
+
+            if (!(declTailGroup.Head is ScriptSymbol declName))
+                throw new FormatException(declTailGroup.Head.ToString());
+
+            var globalName = declName.Value;
+
+            if (!(declTailGroup.Tail is ScriptGroup declTailTailGroup))
+                throw new FormatException(declTailGroup.Tail.ToString());
+
+            var globalInit = CompileExpression(declTailTailGroup.Head);
+
+            Definition.Globals.Add(new ScriptGlobal
+            {
+                Name = globalName,
+                Type = globalType,
+                InitializationExpressionHandle = globalInit
+            });
         }
 
         private void CompileScript(IScriptSyntax node)
         {
-            switch (node)
+            if (!(node is ScriptGroup group) ||
+                !(group.Head is ScriptSymbol symbol && symbol.Value == "script") ||
+                !(group.Tail is ScriptGroup declGroup))
             {
-                case ScriptGroup group:
-                    throw new NotImplementedException();
-
-                default:
-                    throw new FormatException(node.ToString());
+                throw new FormatException(node.ToString());
             }
+
+            var scriptType = CompileScriptType(declGroup.Head);
+
+            if (!(declGroup.Tail is ScriptGroup declTailGroup))
+                throw new FormatException(declGroup.Tail.ToString());
+
+            var scriptReturnType = CompileScriptValueType(declTailGroup.Head);
+
+            if (!(declTailGroup.Tail is ScriptGroup declTailTailGroup))
+                throw new FormatException(declTailGroup.Tail.ToString());
+
+            //
+            // BEGIN TODO: Parse paremeters in possible (script_name (type1 name1) (type2 name2) ...)
+            //
+
+            if (!(declTailTailGroup.Head is ScriptSymbol declName))
+                throw new FormatException(declTailGroup.Head.ToString());
+
+            var scriptName = declName.Value;
+
+            //
+            // END TODO
+            //
+
+            var scriptInit = CompileExpression(declTailTailGroup.Head);
+
+            Definition.Scripts.Add(new Script
+            {
+                ScriptName = scriptName,
+                Type = scriptType,
+                ReturnType = scriptReturnType,
+                RootExpressionHandle = scriptInit,
+                Parameters = new List<ScriptParameter>() // TODO
+            });
+        }
+
+        private uint CompileExpression(IScriptSyntax head)
+        {
+            throw new NotImplementedException();
         }
     }
 }
