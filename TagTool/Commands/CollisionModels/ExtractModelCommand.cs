73a041e46e6fdec7809c72235aba3a8e53a9cc32
Camden Smallwood <camden.smallwood@gmail.com>
2019-04-08 20:37:06 -0400

    Update 'ExtractModel' command for collision models

--- TagTool/Commands/CollisionModels/ExtractModelCommand.cs
+++ TagTool/Commands/CollisionModels/ExtractModelCommand.cs
@@ -1,7 +1,7 @@
-﻿using TagTool.Tags.Definitions;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using TagTool.Tags.Definitions;
 
 namespace TagTool.Commands.CollisionModels
 {
@@ -11,12 +11,12 @@ namespace TagTool.Commands.CollisionModels
 
         public ExtractModelCommand(CollisionModel definition) :
             base(true,
-                
+
                 "ExtractModel",
                 "",
-                
+
                 "ExtractModel <File>",
-                
+
                 "")
         {
             Definition = definition;
@@ -29,28 +29,61 @@ namespace TagTool.Commands.CollisionModels
 
             var file = new FileInfo(args[0]);
 
-            using (var _writer = new StreamWriter(file.Create()))
+            if (!file.Directory.Exists)
+                file.Directory.Create();
+
+            using (var writer = new StreamWriter(file.Create()))
             {
-                _writer.WriteLine("# Extracted on {0}", DateTime.Now);
+                var baseVertex = 0;
 
                 foreach (var region in Definition.Regions)
                 {
                     foreach (var permutation in region.Permutations)
                     {
-                        foreach (var bsp in permutation.Bsps)
+                        foreach (var collisionBsp in permutation.Bsps)
                         {
-                            foreach (var vertex in bsp.Geometry.Vertices)
+                            for (var i = 0; i < collisionBsp.Geometry.Vertices.Count; i++)
                             {
-                                _writer.WriteLine("v {0} {1} {2}", vertex.Point.X, vertex.Point.Y, vertex.Point.Z);
+                                var vertex = collisionBsp.Geometry.Vertices[i];
+                                writer.WriteLine($"v {vertex.Point.X} {vertex.Point.Z} {vertex.Point.Y}");
                             }
 
-                            foreach (var entry in bsp.Geometry.GenerateIndices())
+                            writer.WriteLine($"g bsp_surfaces_{permutation.Bsps.IndexOf(collisionBsp)}");
+
+                            for (var i = 0; i < collisionBsp.Geometry.Surfaces.Count; i++)
                             {
-                                _writer.Write("f");
-                                foreach (var index in entry.Value)
-                                    _writer.Write($" {index + 1}");
-                                _writer.WriteLine();
+                                var surface = collisionBsp.Geometry.Surfaces[i];
+                                var vertices = new HashSet<short>();
+                                var edge = collisionBsp.Geometry.Edges[surface.FirstEdge];
+
+                                writer.Write("f");
+
+                                while (true)
+                                {
+                                    if (edge.LeftSurface == i)
+                                    {
+                                        writer.Write($" {baseVertex + edge.StartVertex + 1}");
+
+                                        if (edge.ForwardEdge == surface.FirstEdge)
+                                            break;
+                                        else
+                                            edge = collisionBsp.Geometry.Edges[edge.ForwardEdge];
+                                    }
+                                    else if (edge.RightSurface == i)
+                                    {
+                                        writer.Write($" {baseVertex + edge.EndVertex + 1}");
+
+                                        if (edge.ReverseEdge == surface.FirstEdge)
+                                            break;
+                                        else
+                                            edge = collisionBsp.Geometry.Edges[edge.ReverseEdge];
+                                    }
+                                }
+
+                                writer.WriteLine();
                             }
+
+                            baseVertex += collisionBsp.Geometry.Vertices.Count;
                         }
                     }
                 }
