82c8d409a50bb17ba9b31d590e93301866cf1d5c
beatthezombie <beatthezombie94@gmail.com>
2019-06-01 10:05:31 -0400

    Add initial animation extractor command with basic layout to converto assimp structure.

---
+++ TagTool/Animations/AnimationExtractor.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assimp;
+using TagTool.Cache;
+using TagTool.Tags.Definitions;
+
+namespace TagTool.Animations
+{
+    /// <summary>
+    /// Animation extraction class to build assimp animations. May have to expose the scene so that models and animations can be merged together.
+    /// </summary>
+    public class AnimationExtractor
+    {
+        private Scene Scene;
+        private readonly HaloOnlineCacheContext CacheContext;
+        private readonly ModelAnimationGraph ModelAnimationGraph;
+        private List<Node> Nodes;
+
+        public AnimationExtractor(ModelAnimationGraph modelAnimationGraph, HaloOnlineCacheContext cacheContext)
+        {
+            ModelAnimationGraph = modelAnimationGraph;
+            CacheContext = cacheContext;
+            Scene = new Scene();
+            for(int i = 0; i< ModelAnimationGraph.SkeletonNodes.Count; i++)
+            {
+                Nodes.Add(new Node(CacheContext.GetString(ModelAnimationGraph.SkeletonNodes[i].Name)));
+            }
+        }
+
+        public void ExtractAnimations(string animationName="*")
+        {
+
+            for(int i = 0; i< ModelAnimationGraph.Animations.Count; i++)
+            {
+                var tagAnimation = ModelAnimationGraph.Animations[i];
+                var name = CacheContext.GetString(tagAnimation.Name);
+
+                if(animationName == "*" || name == animationName)
+                {
+                    Animation animation = new Animation
+                    {
+                        Name = name
+                    };
+
+                    animation.TicksPerSecond = 30;
+                    animation.DurationInTicks = tagAnimation.FrameCount;
+
+                    //
+                    // Convert tag animation into assimp animation. A channel affects only a single node (bone)
+                    //
+
+                    //Animation data should go here
+
+                    for(int j = 0; j< tagAnimation.NodeCount; j++)
+                    {
+                        NodeAnimationChannel channel = new NodeAnimationChannel();
+                        channel.NodeName = Nodes[j].Name;
+
+                        // parse animation data for a specific node
+
+
+                    }
+                    Scene.Animations.Add(animation);
+                }
+            }
+
+
+            
+
+            
+        }
+
+        public NodeAnimationChannel ExtractChannel(AnimationData data)
+        {
+            NodeAnimationChannel channel = new NodeAnimationChannel();
+            channel.NodeName = "";
+
+
+
+
+
+            return null;
+        }
+
+
+
+        public List<VectorKey> GetPositionVectorKeys(AnimationData data)
+        {
+
+            List<VectorKey> positions = new List<VectorKey>();
+
+            for(int i = 0; i< data.Translations.Count; i++)
+            {
+
+            }
+            
+            return null;
+        }
+    }
+}
