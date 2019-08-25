1dc47efe3f0dabbfaab1ca446c0400693bc0ee8b
Hari_ <null@null.com>
2019-08-25 17:45:15 -0400

    fix updatemapfiles command to include ODST maps, firefight, and rally points

--- TagTool/Commands/Files/UpdateMapFilesCommand.cs
+++ TagTool/Commands/Files/UpdateMapFilesCommand.cs
@@ -33,14 +33,14 @@ namespace TagTool.Commands.Files
         {
             CacheContext = cacheContext;
         }
-        
+
         public override object Execute(List<string> args)
         {
-            if(args.Count != 1)
+            if (args.Count > 1)
             {
                 return false;
             }
-            
+
             var scenarioIndices = new Dictionary<int, (ScenarioMapType, int)>();
 
             using (var stream = CacheContext.OpenTagCacheRead())
@@ -64,6 +64,9 @@ namespace TagTool.Commands.Files
             var mainmenuMapFile = CacheContext.Directory.GetFiles("mainmenu.map")[0];
             var guardianMapFile = CacheContext.Directory.GetFiles("guardian.map")[0];
 
+            string[] odstmaps = { "c100", "c200", "h100", "l200", "l300", "sc100", "sc110", "sc120", "sc130", "sc140", "sc150" };
+            var odstlist = new List<string>(odstmaps);
+
             foreach (var entry in scenarioIndices)
             {
                 var scnrTag = CacheContext.GetTag(entry.Value.Item2);
@@ -71,6 +74,10 @@ namespace TagTool.Commands.Files
 
                 var mapName = scenarioPath.Split('\\').Last();
                 var mapFile = new FileInfo(Path.Combine(CacheContext.Directory.FullName, $"{mapName}.map"));
+
+                var isodstmap = false;
+                if (odstlist.Contains(mapName))
+                    isodstmap = true;
 
                 if (!mapFile.Exists)
                     mapFile = ((entry.Value.Item1 == ScenarioMapType.Multiplayer) ? guardianMapFile : mainmenuMapFile).CopyTo(mapFile.FullName);
@@ -83,9 +90,9 @@ namespace TagTool.Commands.Files
                 }
                 else
                 {
-                    if(Directory.Exists("info"))
+                    if (Directory.Exists(Path.Combine(CacheContext.Directory.FullName, "info")))
                     {
-                        mapInfoDir = new DirectoryInfo("info");
+                        mapInfoDir = new DirectoryInfo(Path.Combine(CacheContext.Directory.FullName, "info"));
                     }
                 }
 
@@ -124,6 +131,106 @@ namespace TagTool.Commands.Files
                             using (var infoStream = mapInfoFile.OpenRead())
                             using (var infoReader = new BinaryReader(infoStream))
                             {
+                                if (entry.Value.Item1 == ScenarioMapType.SinglePlayer)
+                                {
+                                    infoStream.Position = 0x0;
+                                    stream.Position = 0x3390;
+                                    writer.Write(infoReader.ReadBytes(0x3C)); //mapinfo header
+                                    //stream.Position = 0x33cc;
+
+                                    writer.Write(entry.Key); //scenario map id
+                                    infoStream.Position = 0x40; //advance infostream past map id
+                                    var zone = infoReader.ReadBytes(4);
+                                    Array.Reverse(zone); //this must be swapped for the map file to work
+                                    writer.Write(zone);
+
+                                    //writer.Write(infoReader.ReadBytes(0x1120)); //cache file metadata
+                                    stream.Position = 0x44f0;
+                                    var infocontentbase = 0x1160;
+                                    var contententry = 0;
+
+                                    //each content entry is 0xF10 in ODST, 0xF08 in H3
+                                    //There are 9 content entries in ODST and 3 in H3
+                                    var contentnames = new byte[12][];
+                                    var contentdescs = new byte[12][];
+                                    if (isodstmap)
+                                    {
+                                        while (contententry < 9)
+                                        {
+                                            infoStream.Position = (infocontentbase + contententry * 0xf10);
+                                            writer.Write(infoReader.ReadBytes(0x10));
+
+                                            for (var i = 0; i < 12; i++)
+                                                contentnames[i] = infoReader.ReadBytes(0x40);
+                                            foreach (var contentnameUnicode in contentnames)
+                                            {
+                                                for (var c = 0; c < contentnameUnicode.Length; c += 2)
+                                                {
+                                                    var temp = contentnameUnicode[c];
+                                                    contentnameUnicode[c] = contentnameUnicode[c + 1];
+                                                    contentnameUnicode[c + 1] = temp;
+                                                }
+                                                writer.Write(contentnameUnicode);
+                                            }
+
+                                            for (var i = 0; i < 12; i++)
+                                            {
+                                                contentdescs[i] = infoReader.ReadBytes(0x100);
+                                            }
+                                            foreach (var contentdesc in contentdescs)
+                                            {
+                                                for (var c = 0; c < contentdesc.Length; c += 2)
+                                                {
+                                                    var temp = contentdesc[c];
+                                                    contentdesc[c] = contentdesc[c + 1];
+                                                    contentdesc[c + 1] = temp;
+                                                }
+
+                                                writer.Write(contentdesc);
+                                            }
+                                            contententry += 1;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        while (contententry < 3)
+                                        {
+                                            infoStream.Position = (infocontentbase + contententry * 0xf08);
+                                            writer.Write(infoReader.ReadBytes(0x8));
+                                            writer.Write(0); //padding
+                                            writer.Write(0); //padding
+                                            for (var i = 0; i < 12; i++)
+                                                contentnames[i] = infoReader.ReadBytes(0x40);
+                                            foreach (var contentnameUnicode in contentnames)
+                                            {
+                                                for (var c = 0; c < contentnameUnicode.Length; c += 2)
+                                                {
+                                                    var temp = contentnameUnicode[c];
+                                                    contentnameUnicode[c] = contentnameUnicode[c + 1];
+                                                    contentnameUnicode[c + 1] = temp;
+                                                }
+                                                writer.Write(contentnameUnicode);
+                                            }
+
+                                            for (var i = 0; i < 12; i++)
+                                            {
+                                                contentdescs[i] = infoReader.ReadBytes(0x100);
+                                            }
+                                            foreach (var contentdesc in contentdescs)
+                                            {
+                                                for (var c = 0; c < contentdesc.Length; c += 2)
+                                                {
+                                                    var temp = contentdesc[c];
+                                                    contentdesc[c] = contentdesc[c + 1];
+                                                    contentdesc[c + 1] = temp;
+                                                }
+
+                                                writer.Write(contentdesc);
+                                            }
+                                            contententry += 1;
+                                        }
+                                    }
+                                }
                                 var mapNames = new byte[12][];
 
                                 infoStream.Position = 0x44;
@@ -198,8 +305,8 @@ namespace TagTool.Commands.Files
                             writer.Write(entry.Key);
                         }
                     }
-                    
-                    if(using_mapinfo)
+
+                    if (using_mapinfo)
                     {
                         Console.WriteLine($"Scenario tag index for {mapFile.Name}: 0x{entry.Value.Item2:X4} (using map info)");
                     }
@@ -207,10 +314,10 @@ namespace TagTool.Commands.Files
                     {
                         Console.WriteLine($"Scenario tag index for {mapFile.Name}: 0x{entry.Value.Item2:X4} (WARNING: not using map info)");
                     }
-                    
+
                 }
             }
-            
+
             Console.WriteLine("Done!");
 
             return true;
