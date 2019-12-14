d14e2ea29bce549e5804cc8e33839631800cfc3f
Hari <hari@theguardians>
2019-12-13 20:50:41 -0500

    update vmdx definition per Pedro13

--- TagTool/Tags/Definitions/VisionMode.cs
+++ TagTool/Tags/Definitions/VisionMode.cs
@@ -6,71 +6,52 @@ namespace TagTool.Tags.Definitions
     [TagStructure(Name = "vision_mode", Tag = "vmdx", Size = 0x188, MinVersion = CacheVersion.Halo3ODST, MaxVersion = CacheVersion.Halo3ODST)]
     [TagStructure(Name = "vision_mode", Tag = "vmdx", Size = 0x1A0, MinVersion = CacheVersion.HaloOnline106708)]
     public class VisionMode : TagStructure
-	{
+    {
         public sbyte Unknown;
         public sbyte Unknown2;
+        public sbyte Flags;
         public sbyte Unknown3;
-        public sbyte Unknown4;
 
-        public float Unknown5;
-        [TagField(MinVersion = CacheVersion.HaloOnline106708)]
-        public float Unknown6;
-        public float Unknown7;
-        public float Unknown8;
-        public float Unknown9;
-        public float Unknown10;
+        public RenderDataBlock ParentRenderData;
+        public RenderDataBlock ChildRenderData;
+        public RenderDataBlock TheaterRenderData;
 
-        public float Unknown11;
-        [TagField(MinVersion = CacheVersion.HaloOnline106708)]
-        public float Unknown12;
-        public float Unknown13;
-        public float Unknown14;
-        public float Unknown15;
-        public float Unknown16;
+        public CachedTagInstance Unknown4;
 
-        public float Unknown17;
-        [TagField(MinVersion = CacheVersion.HaloOnline106708)]
-        public float Unknown18;
-        public float Unknown19;
-        public float Unknown20;
-        public float Unknown21;
-        public float Unknown22;
+        public OutlineDataBlock WeaponOutlineData;
+        public OutlineDataBlock AllyOutlineData;
+        public OutlineDataBlock EnemyOutlineData;
+        public OutlineDataBlock ObjectiveOutlineData;
+        public OutlineDataBlock SceneryOutlineData;
 
-        public CachedTagInstance Unknown23;
-        public TagFunction Unknown24;
-        public TagFunction Unknown25;
-        public float Unknown26;
-        public float Unknown27;
-        public float Unknown28;
-        public float Unknown29;
-        public TagFunction Unknown30;
-        public TagFunction Unknown31;
-        public float Unknown32;
-        public float Unknown33;
-        public float Unknown34;
-        public float Unknown35;
-        public TagFunction Unknown36;
-        public TagFunction Unknown37;
-        public float Unknown38;
-        public float Unknown39;
-        public float Unknown40;
-        public float Unknown41;
-        public TagFunction Unknown42;
-        public TagFunction Unknown43;
-        public float Unknown44;
-        public float Unknown45;
-        public float Unknown46;
-        public float Unknown47;
-        public TagFunction Unknown48;
-        public TagFunction Unknown49;
-        public float Unknown50;
-        public float Unknown51;
-        public float Unknown52;
-        public float Unknown53;
-        public CachedTagInstance Unknown54;
-        public CachedTagInstance Unknown55;
+        public CachedTagInstance VisionModeMask;
+        public CachedTagInstance VisionModeCameraFx;
 
         [TagField(Flags = Padding, Length = 12, MinVersion = CacheVersion.HaloOnline106708)]
         public byte[] Unused;
+
+        [TagStructure(Size = 0x18, MinVersion = CacheVersion.HaloOnline106708)]
+        [TagStructure(Size = 0x14, MinVersion = CacheVersion.Halo3ODST, MaxVersion = CacheVersion.Halo3ODST)]
+        public class RenderDataBlock : TagStructure
+        {
+            public float PrimaryRenderDistance;
+            [TagField(MinVersion = CacheVersion.HaloOnline106708)]
+            public float SecondaryRenderDistance;
+            public float AnimationSpeed;
+            public float AnimationTimeModifier;
+            public float Unknown;
+            public float AnimationDelayBySeconds;
+        }
+
+        [TagStructure(Size = 0x38, MinVersion = CacheVersion.Halo3ODST, MaxVersion = CacheVersion.HaloOnline106708)]
+        public class OutlineDataBlock : TagStructure
+        {
+            public TagFunction PrimaryColourFunction;
+            public TagFunction SecondaryColourFunction;
+            public float FillIntensity;
+            public float PrimaryColourInfluence;
+            public float SecondaryColourInfluence;
+            public float OutlineIntensity;
+        }
     }
 }
