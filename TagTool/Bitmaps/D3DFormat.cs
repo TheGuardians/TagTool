0f892d41cd9971a07e8ddb1284d5a974128f9dcc
beatthezombie <beatthezombie94@gmail.com>
2019-12-25 18:50:17 -0800

    Add TagResourceReference and D3Dformat for xbox

--- TagTool/Bitmaps/D3DFormat.cs
+++ TagTool/Bitmaps/D3DFormat.cs
@@ -7,15 +7,7 @@ using System.Threading.Tasks;
 namespace TagTool.Bitmaps
 {
     // TODO: fill the values using the resource definition and the bitmap tag definition.
-    public enum D3DFormatXbox : byte
+    public enum D3DFormat : byte
     {
-        A8 = 0x2,
-        A8Y8  = 0x4A,
-        A8R8G8B8 = 0xB2,
-        Dxn = 0x71,
-        Dxt1 = 0x73,
-        Dxt3 = 0x74,
-        Dxt5 = 0x75,
-        Ctx1 = 0x7C
     }
 }
---
+++ TagTool/Tags/TagResourceReference.cs
@@ -0,0 +1,21 @@
+﻿using TagTool.Cache;
+using TagTool.Common;
+
+namespace TagTool.Tags
+{
+    [TagStructure(Size = 0x4)]
+    public class TagResourceReference
+    {
+        /// <summary>
+        /// ID is an index in ResourceGestalt.TagResources
+        /// </summary>
+        [TagField(Gen = CacheGeneration.Third)]
+        public DatumIndex Gen3ResourceID;
+
+        /// <summary>
+        /// PageableResource structure (as a pointer)
+        /// </summary>
+        [TagField(Gen = CacheGeneration.HaloOnline, Flags = TagFieldFlags.Pointer)]
+        public PageableResource HaloOnlinePageableResource;
+    }
+}
