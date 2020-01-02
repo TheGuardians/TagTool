ef60a0f07b0df902847d0bd2dc7c7a2d28f8325c
beatthezombie <beatthezombie94@gmail.com>
2020-01-02 11:22:28 -0800

    Add D3D Format for Xbox and PC according to tag data

--- TagTool/Bitmaps/D3DFormat.cs
+++ TagTool/Bitmaps/D3DFormat.cs
@@ -9,5 +9,11 @@ namespace TagTool.Bitmaps
     // TODO: fill the values using the resource definition and the bitmap tag definition.
     public enum D3DFormat : byte
     {
+        D3DFMT_UNKNOWN = 0x0, // All A8, A8Y8, Y8, A8R8G8B8 are contained in this one...
+        D3DFMT_DXT1 = 0x31,
+        D3DFMT_ATI2 = 0x32,
+        D3DFMT_DXT3 = 0x33,
+        D3DFMT_DXT4 = 0x34,
+        D3DFMT_DXT5 = 0x35
     }
 }
