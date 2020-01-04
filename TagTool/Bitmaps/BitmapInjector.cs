bd8d98ca3ec8bf73b6c1c94c184d0b6a9868835d
Beatthezombie <beatthezombie96@gmail.com>
2020-01-04 08:59:19 -0800

    some work on bitmap injection with new system

---
+++ TagTool/Bitmaps/BitmapInjector.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TagTool.Bitmaps.DDS;
+using TagTool.Cache;
+using TagTool.Tags;
+using TagTool.Tags.Resources;
+
+namespace TagTool.Bitmaps
+{
+    public class BitmapInjector
+    {
+
+        
+
+        public static BitmapTextureInteropResourceTest CreateBitmapResourceFromDDS(GameCache cache, DDSFile file)
+        {
+            BitmapTextureInteropResourceTest result = new BitmapTextureInteropResourceTest
+            {
+                Texture = new D3DStructure<BitmapTextureInteropResourceTest.BitmapDefinition>
+                {
+                    Definition = new BitmapTextureInteropResourceTest.BitmapDefinition()
+                }
+            };
+
+            
+
+            if(cache.GetType() == typeof(GameCacheContextHaloOnline))
+            {
+                result.Texture.Definition.PrimaryResourceData.Data = file.BitmapData;
+            }
+            else if(cache.GetType() == typeof(GameCacheContextGen3))
+            {
+                // need to do some serious conversion, might be better to require an uncompressed input
+                throw new NotImplementedException();
+            }
+
+            
+            
+            return result;
+        }
+    }
+}
