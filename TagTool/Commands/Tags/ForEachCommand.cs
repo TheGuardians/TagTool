0d4b4e4ac77206ad955e5d97167b89517b1d6fe6
unk-1 <unk0x4@gmail.com>
2020-01-12 07:03:25 +0000

    Add multi-line support to ForEach commands

--- TagTool/Commands/Tags/ForEachCommand.cs
+++ TagTool/Commands/Tags/ForEachCommand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using TagTool.Cache;
@@ -30,7 +31,7 @@ namespace TagTool.Commands.Tags
 
         public override object Execute(List<string> args)
         {
-            if (args.Count < 2)
+            if (args.Count < 1)
                 return false;
 
             var isConst = false;
@@ -41,7 +42,7 @@ namespace TagTool.Commands.Tags
                 isConst = true;
             }
 
-            if (args.Count < 2)
+            if (args.Count < 1)
                 return false;
 
             if (!CacheContext.TryParseGroupTag(args[0], out var groupTag))
@@ -55,13 +56,18 @@ namespace TagTool.Commands.Tags
             var startFilter = "";
             var endFilter = "";
             var filter = "";
+            var filename = "";
 
             string pattern = null;
 
-            while (args[0].EndsWith(":"))
+            while (args.Count > 0 && args[0].EndsWith(":"))
             {
                 switch (args[0].ToLower())
                 {
+                    case "in_file:":
+                        filename = args[1];
+                        args.RemoveRange(0, 2);
+                        break;
                     case "regex:":
                         if (args.Count < 3)
                             return false;
@@ -103,11 +109,44 @@ namespace TagTool.Commands.Tags
                 }
             }
 
+            var commandsToExecute = new List<List<string>>();
+
+            // if no command is given, keep reading commands from stdin until an empty line encountered
+            if (args.Count < 1)
+            {
+                string line;
+                while (!string.IsNullOrWhiteSpace(line = Console.ReadLine()))
+                {
+                    var commandsArgs = ArgumentParser.ParseCommand(line, out string redirectFile);
+                    commandsToExecute.Add(commandsArgs);
+                }
+            }
+            else
+            {
+                commandsToExecute.Add(args);
+            }
+
+            IEnumerable<CachedTagInstance> tags = null;
+
+            // if a file is given use that as the source for tags
+            if (!string.IsNullOrWhiteSpace(filename))
+            {
+                var tagsList = new List<CachedTagInstance>();
+                foreach (var line in File.ReadAllLines(filename))
+                    tagsList.Add(CacheContext.GetTag(line));
+
+                tags = tagsList;
+            }
+            else
+            {
+                tags = CacheContext.TagCache.Index;
+            }
+               
             var rootContext = ContextStack.Context;
 
             using (var stream = CacheContext.OpenTagCacheReadWrite())
             {
-                foreach (var instance in CacheContext.TagCache.Index)
+                foreach (var instance in tags)
                 {
                     if (instance == null || (groupTag != Tag.Null && !instance.IsInGroup(groupTag)))
                         continue;
@@ -132,7 +171,9 @@ namespace TagTool.Commands.Tags
 
                     Console.WriteLine();
                     Console.WriteLine($"{tagName}.{CacheContext.GetString(instance.Group.Name)}:");
-                    ContextStack.Context.GetCommand(args[0]).Execute(args.Skip(1).ToList());
+
+                    foreach (var command in commandsToExecute)
+                        ContextStack.Context.GetCommand(command[0]).Execute(command.Skip(1).ToList());
 
                     while (ContextStack.Context != rootContext) ContextStack.Pop();
 
