86507f842575e5f7f3b1dae4ca71690f1f8f3e4f
Pedro13 <pfroon1@gmail.com>
2020-03-03 16:46:33 +1100

    update terminal definition

--- TagTool/Tags/Definitions/Terminal.cs
+++ TagTool/Tags/Definitions/Terminal.cs
@@ -1,3 +1,4 @@
+using System;
 using TagTool.Cache;
 using TagTool.Common;
 
@@ -6,30 +7,66 @@ namespace TagTool.Tags.Definitions
     [TagStructure(Name = "device_terminal", Tag = "term", Size = 0x140, MinVersion = CacheVersion.Halo3Retail)]
     public class Terminal : Device
     {
-        public uint Unknown1;
+        [TagField(Flags = TagFieldFlags.Padding, Length = 0x4)]
+        public byte[] Unused1;
+
         public StringId ActionString;
         public short TerminalIndex;
-        public short Unknown2;
-        public float Unknown3;
+
+        [TagField(Flags = TagFieldFlags.Padding, Length = 0x2)]
+        public byte[] Unused2;
+
+        public float TerminalExposure;
         public CachedTag ActivationSound;
         public CachedTag DeactivationSound;
         public CachedTag TranslateSound1;
         public CachedTag TranslateSound2;
         public CachedTag ErrorSound;
 
-        [TagField(Length = 4)]
-        public TerminalScreen[] TerminalScreens = new TerminalScreen[4];
+        public TerminalScreen EasyScreen;
+        public TerminalScreen NormalScreen;
+        public TerminalScreen HeroicScreen;
+        public TerminalScreen LegendaryScreen;
 
         [TagStructure(Size = 0x38)]
         public class TerminalScreen : TagStructure
         {
             public CachedTag DummyStrings;
-            public CachedTag TerminalStrings;
-            public short ScreenUnknown1;
-            public short ScreenUnknown2;
-            public short ScreenUnknown3;
-            public short ScreenUnknown4;
+            public CachedTag StoryStrings;
+            public DialectTypeFlags DummyDialectTypes;
+            public DialectTypeFlags StoryDialectTypes;
+            public TerminalImage DummyBlfImage;
+            public TerminalImage StoryBlfImage;
             public CachedTag ErrorStrings;
+
+            [Flags]
+            public enum DialectTypeFlags : short
+            {
+                None = 0,
+                Military = 1 << 0,
+                Public = 1 << 1,
+                Private = 1 << 2,
+                Librarian = 1 << 3,
+                Didact = 1 << 4,
+                Mendicant = 1 << 5,
+                Offensive = 1 << 6,
+                Gravemind = 1 << 7,
+                Fleet = 1 << 8,
+                Spark = 1 << 9
+            }
+
+            public enum TerminalImage : short
+            {
+                None = -1,
+                Image1,
+                Image2,
+                Image3,
+                Image4,
+                Image5,
+                Image6,
+                Image7,
+                Image8
+            }
         }
     }
 }
