2802985417d9ccab8c4068f1a896729bea6678b3
Beatthezombie <beatthezombie96@gmail.com>
2020-04-03 18:22:57 -0700

    testcommand updated to extract all shaders for reversing

--- TagTool/Commands/Tags/TestCommand.cs
+++ TagTool/Commands/Tags/TestCommand.cs
@@ -19,6 +19,7 @@ using TagTool.Havok;
 using System.Linq;
 using System.IO.Compression;
 using TagTool.Tools.Geometry;
+using TagTool.Shaders;
 
 namespace TagTool.Commands
 {
@@ -58,6 +59,61 @@ namespace TagTool.Commands
 
             using (var stream = Cache.OpenCacheRead())
             {
+                // disassemble specified shaders related to rmt2
+                var tagName = @"shaders\shader_templates\_0_0_0_0_0_0_0_0_0_0_0";
+
+                var rmt2Tag = Cache.TagCache.GetTag(tagName, "rmt2");
+                var glvsTag = Cache.TagCache.GetTag(@"shaders\shader_shared_vertex_shaders", "glvs");
+                var rmt2 = Cache.Deserialize<RenderMethodTemplate>(stream, rmt2Tag);
+                var glvs = Cache.Deserialize<GlobalVertexShader>(stream, glvsTag);
+
+                var pixl = Cache.Deserialize<PixelShader>(stream, rmt2.PixelShader);
+
+                Directory.CreateDirectory(tagName);
+                // get vertex shaders (TODO: check vtsh if it has an overriding shader for the entry point?)
+                foreach(VertexType vertex in Enum.GetValues(typeof(VertexType)))
+                {
+                    if( (int)vertex < glvs.VertexTypes.Count)
+                    {
+                        var currentVertex = glvs.VertexTypes[(int)vertex];
+                        
+                        foreach (EntryPoint entry in Enum.GetValues(typeof(EntryPoint)))
+                        {
+                            if ((int)entry < currentVertex.DrawModes.Count)
+                            {
+                                var entryShader = currentVertex.DrawModes[(int)entry].ShaderIndex;
+
+                                if (entryShader != -1)
+                                {
+                                    string entryName = entry.ToString() + ".vertex_shader";
+                                    string vertexFolderName = Path.Combine(tagName, vertex.ToString().ToLower());
+                                    Directory.CreateDirectory(vertexFolderName);
+                                    string vertexShaderFilename = Path.Combine(vertexFolderName, entryName);
+
+                                    DisassembleHaloOnlineShader(glvs, entryShader, vertexShaderFilename);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                foreach (EntryPoint entry in Enum.GetValues(typeof(EntryPoint)))
+                {
+                    if ((int)entry < pixl.DrawModes.Count)
+                    {
+                        var entryShader = pixl.DrawModes[(int)entry].Offset;
+
+                        if (pixl.DrawModes[(int)entry].Count != 0)
+                        {
+                            string entryName = entry.ToString().ToLower() + ".pixel_shader";
+                            string pixelShaderFilename = Path.Combine(tagName, entryName);
+
+                            DisassembleHaloOnlineShader(pixl, entryShader, pixelShaderFilename);
+                        }
+                    }
+                }
+
+                /*
                 //objects\gear\human\industrial\toolbox_small\toolbox_small
                 var tag = Cache.TagCache.GetTag(@"objects\vehicles\warthog\warthog", "mode"); // objects\vehicles\warthog\warthog
                 var mode = Cache.Deserialize<RenderModel>(stream, tag);
@@ -70,12 +126,77 @@ namespace TagTool.Commands
                 using (var writer = new EndianWriter(modelStream))
                 {
                     geometryFormat.SerializeToFile(writer);
-                }
+                }*/
             }
             return true;
         }
 
-        
+
+        private string DisassembleHaloOnlineShader(object definition, int shaderIndex, string filename)
+        {
+            string disassembly = null;
+
+            if (definition.GetType()  == typeof(PixelShader) || definition.GetType() == typeof(GlobalPixelShader))
+            {
+                PixelShaderBlock shader_block = null;
+                if (definition.GetType() == typeof(PixelShader))
+                {
+                    var _definition = definition as PixelShader;
+                    if (shaderIndex < _definition.Shaders.Count)
+                        shader_block = _definition.Shaders[shaderIndex];
+                    else
+                        return null;
+                }
+
+                if (definition.GetType() == typeof(GlobalPixelShader))
+                {
+                    var _definition = definition as GlobalPixelShader;
+                    if (shaderIndex < _definition.Shaders.Count)
+                        shader_block = _definition.Shaders[shaderIndex];
+                    else
+                        return null;
+                }
+
+                var pc_shader = shader_block.PCShaderBytecode;
+                disassembly = D3DCompiler.Disassemble(pc_shader);
+                if (pc_shader == null)
+                    disassembly = null;
+            }
+
+            if (definition.GetType() == typeof(VertexShader) || definition.GetType() == typeof(GlobalVertexShader))
+            {
+                VertexShaderBlock shader_block = null;
+                if (definition.GetType() == typeof(VertexShader))
+                {
+                    var _definition = definition as VertexShader;
+                    if (shaderIndex < _definition.Shaders.Count)
+                        shader_block = _definition.Shaders[shaderIndex];
+                    else
+                        return null;
+                }
+
+                if (definition.GetType() == typeof(GlobalVertexShader))
+                {
+                    var _definition = definition as GlobalVertexShader;
+                    if (shaderIndex < _definition.Shaders.Count)
+                        shader_block = _definition.Shaders[shaderIndex];
+                    else
+                        return null;
+                }
+
+                var pc_shader = shader_block.PCShaderBytecode;
+                disassembly = D3DCompiler.Disassemble(pc_shader);
+                if (pc_shader == null)
+                    disassembly = null;
+            }
+
+            using (var writer = File.CreateText(filename))
+            {
+                writer.WriteLine(disassembly);
+            }
+
+            return disassembly;
+        }
     }
 }
 
