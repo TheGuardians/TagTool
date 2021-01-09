0b9ce90076181f6866d2fca9cb0eccaaaafdfa00
Vegetal-Pole <vegetal.halo@gmail.com>
2021-01-09 14:42:21 -0600

    Optional name assignment when duplicating tag
    If 2 args are provided the second will be the duplicate tag's name. Use savetagnames some time after.

--- TagTool/Commands/Tags/DuplicateTagCommand.cs
+++ TagTool/Commands/Tags/DuplicateTagCommand.cs
@@ -23,21 +23,29 @@ namespace TagTool.Commands.Tags
 
         public override object Execute(List<string> args)
         {
-            if (args.Count < 1)
+            if (args.Count < 1 || args.Count > 2)
                 return new TagToolError(CommandError.ArgCount);
             if (!Cache.TagCache.TryGetCachedTag(args[0], out var originalTag))
                 return new TagToolError(CommandError.TagInvalid);
 
             var newTag = Cache.TagCache.AllocateTag(originalTag.Group);
+			if (args.Count == 2)
+				newTag = Cache.TagCache.AllocateTag(originalTag.Group, args[1]);
 
-            using (var stream = Cache.OpenCacheReadWrite())
+			using (var stream = Cache.OpenCacheReadWrite())
             {
                 var originalDefinition = Cache.Deserialize(stream, originalTag);
                 Cache.Serialize(stream, newTag, originalDefinition);
-            }
 
-            Console.Write($"Tag duplicated to 0x{newTag.Index.ToString("X")}\n");
-            return true;
+				//Cache.SaveTagNames();
+			}
+
+			if (args.Count == 2)
+				Console.WriteLine($"[Index: 0x{newTag.Index:X4}] {args[1]}.{newTag.Group}");
+			else
+				Console.Write($"Tag duplicated to 0x{newTag.Index.ToString("X")}\n");
+
+			return true;
         }
     }
 }
