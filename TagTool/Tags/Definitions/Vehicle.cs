dc870624a6280ba7594c29ad7a05896e0b721571
Hari <hari@theguardians>
2021-03-03 16:27:17 -0500

    update vehi VTOL properties

--- TagTool/Tags/Definitions/Vehicle.cs
+++ TagTool/Tags/Definitions/Vehicle.cs
@@ -473,29 +473,29 @@ namespace TagTool.Tags.Definitions
             public StringId RightLiftMarker;
             public StringId ThrustMarker;
 
-            public Angle Unknown2;
-            public Angle Unknown3;
-            public Angle Unknown4;
-            public Angle Unknown5;
-            public float Unknown6;
-            public float Unknown7;
-            public float Unknown8;
-            public float Unknown9;
-            public float Unknown10;
-            public float Unknown11;
-            public float Unknown12;
-            public float Unknown13;
-            public float Unknown14;
-            public float Unknown15;
-            public float Unknown16;
-            public float Unknown17;
-            public float Unknown18;
-            public float Unknown19;
-            public float Unknown20;
-            public Angle Unknown21;
-            public Angle Unknown22;
-            public float Unknown23;
-            public float Unknown24;
+            public Angle PitchUpRangeMin;
+            public Angle PitchUpRangeMax;
+            public Angle PitchDownRangeMin;
+            public Angle PitchDownRangeMax;
+            public float LiftDistance;
+            public float MaximumUpwardSpeed;
+            public float MaximumDownAcceleration;
+            public float MaximumUpAcceleration;
+            public float MaximumTurnAcceleration;
+            public float TurnAccelerationGain;
+            public float RotorDampening;
+            public float MaximumLeftAcceleration;
+            public float MaximumForwardAcceleration;
+            public float LiftArmPivotLength;
+            public float DragCoefficient;
+            public float ConstantDeceleration;
+            public float MagicAngularAccelerationExponent;
+            public float MagicAngularAccelerationScale;
+            public float MagicAngularAccelerationK;
+            public Angle LiftAnglesAcceleration;
+            public Angle RenderLiftAnglesAcceleration;
+            public float PropellerRotationSpeedMin;
+            public float PropellerRotationSpeedMax;
         }
 
         [TagStructure(Size = 0x70)]
