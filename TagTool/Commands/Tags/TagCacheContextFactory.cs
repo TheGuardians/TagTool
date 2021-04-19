20b707ecc79956556580dc86fa4bcea9f5cd9b77
Vegetal-Pole <vegetal.halo@gmail.com>
2021-04-19 09:30:59 -0500

    Revert "ImportBitmapCommand for importing from base context"
    This reverts commit 57fb3fcd6f86780476ab252e8aa476b0fb11e693.

--- TagTool/Commands/Tags/TagCacheContextFactory.cs
+++ TagTool/Commands/Tags/TagCacheContextFactory.cs
@@ -75,11 +75,10 @@ namespace TagTool.Commands.Tags
                 context.AddCommand(new ExportTagModCommand(hoCache));
                 context.AddCommand(new GenerateShaderCommand(hoCache));
                 context.AddCommand(new GenerateRenderMethodCommand(hoCache));
-                context.AddCommand(new GenerateBitmapCommand(hoCache));
-                context.AddCommand(new ImportBitmapCommand(hoCache));
+				context.AddCommand(new GenerateBitmapCommand(hoCache));
 
-                // modding commands
-                context.AddCommand(new OpenModPackageCommand(contextStack, hoCache));
+				// modding commands
+				context.AddCommand(new OpenModPackageCommand(contextStack, hoCache));
                 context.AddCommand(new CreateCharacterType(cache));
 
                 
