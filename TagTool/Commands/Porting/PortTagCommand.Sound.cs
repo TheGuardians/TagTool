1e3d5bbcd36482fdaf853c333acd339dc7c43d71
Hari <hari@theguardians>
2021-04-19 11:31:53 -0400

    update sound facial animation block definitions and fix mouth data conversion

--- TagTool/Commands/Porting/PortTagCommand.Sound.cs
+++ TagTool/Commands/Porting/PortTagCommand.Sound.cs
@@ -238,16 +238,6 @@ namespace TagTool.Commands.Porting
                 foreach (var section in BlamSoundGestalt.ExtraInfo[sound.SoundReference.ExtraInfoIndex].EncodedPermutationSections)
                 {
                     var newSection = section.DeepClone();
-
-                    foreach (var info in newSection.SoundDialogueInfo)
-                    {
-                        for (var i = ((info.MouthDataLength % 2) == 0 ? 0 : 1); (i + 1) < info.MouthDataLength; i += 2)
-                            Array.Reverse(newSection.EncodedData, (int)(info.MouthDataOffset + i), 2);
-
-                        for (var i = ((info.LipsyncDataLength % 2) == 0 ? 0 : 1); (i + 1) < info.LipsyncDataLength; i += 2)
-                            Array.Reverse(newSection.EncodedData, (int)(info.LipsyncDataOffset + i), 2);
-                    }
-
                     extraInfo.EncodedPermutationSections.Add(newSection);
                 }
             }
