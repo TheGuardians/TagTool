603b9d5f7f395e7b987aae866c3dcd68bed7836c
Hari <hari@theguardians>
2021-06-14 19:10:51 -0400

    fix h4 stringids

--- TagTool/Cache/Gen4/StringIdResolverHalo4.cs
+++ TagTool/Cache/Gen4/StringIdResolverHalo4.cs
@@ -3,13 +3,14 @@
 namespace TagTool.Cache
 {
     /// <summary>
-    /// StringID resolver for Halo 4. Halo Reach+ StringId are 7-8-17 (length-set-index) format instead of (8-8-16)
+    /// StringID resolver for Halo 4. 
+    /// Halo 4 StringId are 5-8-19 (length-set-index) format instead of (8-8-16)
     /// </summary>
     public class StringIdResolverHalo4 : StringIdResolver
     {
-        private static readonly int[] SetOffsets = { 0x20BD, 0x631, 0xEA6, 0xFB1, 0x103E, 0x1136, 0x117D, 0x11A8, 0x1C95, 0x1EA1, 0x1ED4, 0x1F57, 0x1F8B, 0x1F98, 0x1FDA };
+        private static readonly int[] SetOffsets = { 0x20BD, 0x631, 0xEA6, 0xFB1, 0x103E, 0x1136, 0x117D, 0x11A8, 0x1C95, 0x1EA1, 0x1ED4, 0x1ED4, 0x1F57, 0x1F8B, 0x1F98, 0x1FDA };
         private const int SetMin = 0x631;   // Mininum index that goes in a set
-        private const int SetMax = 0x1FFFF; // Maximum index that goes in a set
+        private const int SetMax = 0x7FFFF; // Maximum index that goes in a set
 
         public StringIdResolverHalo4()
         {
