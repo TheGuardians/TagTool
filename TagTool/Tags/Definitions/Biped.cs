0865a9cfb0e2a3cffc8e3af012f145d0a87263c1
Vegetal-Pole <vegetal.halo@gmail.com>
2021-06-17 14:49:23 -0500

    Name bipd unknowns, correct types

--- TagTool/Tags/Definitions/Biped.cs
+++ TagTool/Tags/Definitions/Biped.cs
@@ -16,9 +16,9 @@ namespace TagTool.Tags.Definitions
         public BipedFlagBits BipedFlags;
         public Angle StationaryTurningSpeed;
         [TagField(MinVersion = CacheVersion.Halo3ODST)]
-        public uint Unknown20;
+        public uint Unknown;
         [TagField(MinVersion = CacheVersion.Halo3ODST)]
-        public StringId Unknown21;
+        public StringId RagdollRegionName;
         public float JumpVelocity;
         public float MaximumSoftLandingTime;
         public float MinimumHardLandingTime;
@@ -68,19 +68,19 @@ namespace TagTool.Tags.Definitions
         public float LockonDistance;
 
         public short PhysicsControlNodeIndex;
-        public short Unknown29;
+        public short Unknown1;
 
-        public uint Unknown30;
-        public uint Unknown31;
+        public float CosineStationaryTurningThreshold;
+        public float CrouchTransitionVelocity;
 
         [TagField(MinVersion = CacheVersion.Halo3ODST)]
-        public uint Unknown32;
+        public float CameraHeightVelocity;
 
         public short PelvisNodeIndex;
         public short HeadNodeIndex;
 
         [TagField(MinVersion = CacheVersion.Halo3ODST)]
-        public uint Unknown33;
+        public uint Unknown2;
 
         public float HeadshotAccelerationScale;
         public CachedTag AreaDamageEffect;
@@ -92,17 +92,17 @@ namespace TagTool.Tags.Definitions
         public List<MovementGateBlock> MovementGatesCrouching;
 
         [TagField(MinVersion = CacheVersion.Halo3ODST)]
-        public uint Unknown36;
+        public float JumpAimOffsetDistance;
         [TagField(MinVersion = CacheVersion.Halo3ODST)]
-        public uint Unknown37;
+        public float JumpAimOffsetDuration;
         [TagField(MinVersion = CacheVersion.Halo3ODST)]
-        public uint Unknown38;
+        public float LandAimOffsetDistance;
         [TagField(MinVersion = CacheVersion.Halo3ODST)]
-        public uint Unknown39;
+        public float LandAimOffsetDuration;
         [TagField(MinVersion = CacheVersion.Halo3ODST)]
-        public uint Unknown40;
+        public float AimCompensateMinimumDistance;
         [TagField(MinVersion = CacheVersion.Halo3ODST)]
-        public uint Unknown41;
+        public float AimCompensateMaximumDistance;
 
         public BipedPhysicsFlags PhysicsFlags;
         public float HeightStanding;
@@ -123,16 +123,16 @@ namespace TagTool.Tags.Definitions
         public Angle UphillCutoffAngle;
         public float DownhillVelocityScale;
         public float UphillVelocityScale;
-        public float Unknown42;
-        public float Unknown43;
-        public float Unknown44;
-        public float Unknown45;
-        public float Unknown46;
-        public float Unknown47;
-        public float Unknown48;
-        public float Unknown49;
-        public float Unknown50;
-        public float Unknown51;
+        public float RuntimeMinimumNormalK;
+        public float RuntimeDownhillK0;
+        public float RuntimeDownhillK1;
+        public float RuntimeUphillK0;
+        public float RuntimeUphillK1;
+        public Angle ClimbInflectionAngle;
+        public float ScaleAirborneReactionTime;
+        public float ScaleGroundAdhesionVelocity;
+        public float GravityScale;
+        public float AirborneAccelerationScale;
         public Angle BankAngle;
         public float BankApplyTime;
         public float BankDecayTime;
@@ -150,41 +150,41 @@ namespace TagTool.Tags.Definitions
         public CachedTag TransformationMuffin;
         public CachedTag DeathSpawnCharacter;
         public short DeathSpawnCount;
-        public short Unknown52;
+        public short Unknown3;
 
-        public uint Unknown53;
+        [Flags]
+        public enum BipedLeapFlags : uint
+        {
+            None,
+            ForceEarlyRoll = 1 << 0
+        }
 
-        public float Unknown54;
-        public float Unknown55;
-        public float Unknown56;
-        public float Unknown57;
+        public BipedLeapFlags LeapFlags;
+        public float DampeningScale;
+        public float RollDelay;
+        public float CannonballOffAxisScale;
+        public float CannonballOffTrackScale;
+        public Bounds<Angle> CannonballRollBounds;
+        public Bounds<float> AnticipationRatioBounds;
+        public Bounds<float> ReactionForceBounds;
+        public float LobbingDesire;
 
-        public Angle Unknown58;
-        public Angle Unknown59;
+        public float GroundNormalDampening;
+        public float RootOffsetMaxScale;
+        public float RootOffsetDampening;
+        public float FollowingCamScale;
+        public float RootLeaningScale;
+        public Angle FootRollMax;
+        public Angle FootPitchMax;
 
-        public uint Unknown60;
-        public uint Unknown61;
-
-        public float Unknown62;
-        public float Unknown63;
-        public float Unknown64;
-        public float Unknown65;
-        public float Unknown66;
-        public float Unknown67;
-        public float Unknown68;
-        public float Unknown69;
-
-        public Angle Unknown70;
-        public Angle Unknown71;
-
-        public float Unknown72;
-        public float Unknown73;
-        public float Unknown74;
-        public float Unknown75;
-        public float Unknown76;
+        public float PivotOnFootScale;
+        public float PivotMinFootDelta;
+        public float PivotStrideLengthScale;
+        public float PivotThrottleScale;
+        public float PivotOffsetDampening;
         
         [TagField(MinVersion = CacheVersion.HaloOnline106708)]
-        public uint Unknown77;
+        public uint Unknown4;
 
         [Flags]
         public enum BipedFlagBits : int
@@ -218,9 +218,9 @@ namespace TagTool.Tags.Definitions
             public StringId Class;
             public float StandingHeightFraction;
             public float CrouchingHeightFraction;
-            public uint Unknown;
-            public uint Unknown2;
-            public uint Unknown3;
+            public uint Unknown5;
+            public uint Unknown6;
+            public uint Unknown7;
         }
 
         [Flags]
