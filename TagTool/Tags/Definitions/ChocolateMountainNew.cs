5494ac944d813c68291b33570253340622f86177
NoShotz <noshotz@protonmail.com>
2021-08-10 19:03:35 -0400

    reach chmt definition

--- TagTool/Tags/Definitions/ChocolateMountainNew.cs
+++ TagTool/Tags/Definitions/ChocolateMountainNew.cs
@@ -1,16 +1,43 @@
 using System.Collections.Generic;
+using TagTool.Cache;
 
 namespace TagTool.Tags.Definitions
 {
-    [TagStructure(Name = "chocolate_mountain_new", Tag = "chmt", Size = 0xC)]
+    [TagStructure(Name = "chocolate_mountain_new", Tag = "chmt", Size = 0xC, MaxVersion = CacheVersion.HaloOnline700123)]
+    [TagStructure(Name = "chocolate_mountain_new", Tag = "chmt", Size = 0x10, MinVersion = CacheVersion.HaloReach)]
     public class ChocolateMountainNew : TagStructure
 	{
+		[TagField(MinVersion = CacheVersion.HaloReach)]
+		public sbyte Unknown1;
+		
+		[TagField(MinVersion = CacheVersion.HaloReach)]
+		public sbyte Unknown2;
+		
+		[TagField(MinVersion = CacheVersion.HaloReach)]
+		public sbyte Unknown3;
+		
+		[TagField(MinVersion = CacheVersion.HaloReach)]
+		public sbyte Unknown4;
+		
         public List<LightingVariable> LightingVariables;
 
-        [TagStructure(Size = 0x4)]
+        [TagStructure(Size = 0x4, MaxVersion = CacheVersion.HaloOnline700123)]
+        [TagStructure(Size = 0x14, MinVersion = CacheVersion.HaloReach)]
         public class LightingVariable : TagStructure
 		{
             public float LightmapBrightnessOffset;
+			
+			[TagField(MinVersion = CacheVersion.HaloReach)]
+			public float LightmapDarknessOffset;
+			
+			[TagField(MinVersion = CacheVersion.HaloReach)]
+			public float Unknown;
+			
+			[TagField(MinVersion = CacheVersion.HaloReach)]
+			public float DynamicLightmapBrightnessOffset;
+			
+			[TagField(MinVersion = CacheVersion.HaloReach)]
+			public float DynamicLightmapDarknessOffset;
         }
     }
 }
