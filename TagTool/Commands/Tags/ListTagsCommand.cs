c7552f5842c92828441df065b27d3f1f35c9905b
dt192 <2612048+dt192@users.noreply.github.com>
2021-08-23 04:45:02 +0100

    Added functionality, fixed bugs, simplified.

--- TagTool/Commands/Tags/ListTagsCommand.cs
+++ TagTool/Commands/Tags/ListTagsCommand.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TagTool.Cache;
 using TagTool.Common;
 using TagTool.Commands.Common;
+using System.Linq;
 
 namespace TagTool.Commands.Tags
 {
@@ -14,117 +15,112 @@ namespace TagTool.Commands.Tags
             : base(true,
 
                   "ListTags",
-                  "Lists tag instances that are of the specified tag group.",
+                  "Lists tag instances that are of the specified tag groups.",
 
-                  "ListTags <Tag Group> {Options}",
+                  "ListTags [TagGroup]... {Children:True|FilterType:String}",
 
-                  "Lists tag instances that are of the specified tag group." +
-                  "Multiple group tags to list tags from can be specified.\n" +
-                  "Tags of a group which inherit from the given group tags will also\n" +
-                  "be printed. If no group tag is specified, all tags in the current\n" +
-                  "tag cache file will be listed.")
+                  "e.g. ListTags scnr scenario_structure_bsp contains:unk\n" +
+                  "e.g. ListTags ends:world sbsp scnr starts:levels\\dlc\n" +
+                  "e.g. ListTags has: eli obje children: true has: equi\n\n" +
+
+                  "- If no tag group is specified, all tags will be listed.\n" +
+                  "- Accepts multiple has/contains filters to narrow results.\n" +
+                  "- Multiple tag groups can be specified to search from.\n" +
+                  "- Children:True matches tags inheriting from specified groups.")
         {
             Cache = cache;
         }
 
         public override object Execute(List<string> args)
         {
-            var groupTag = (args.Count == 0 || args[0].EndsWith(":")) ? Tag.Null : Tag.Parse(Cache, args[0]);
-
-            if (groupTag == "{????}")
-            {
-                return new TagToolError(CommandError.ArgInvalid, $"\"{args[0]}\"");
-            }
-
-            if (args.Count > 0 && !args[0].EndsWith(":"))
-                args.RemoveAt(0);
-
-            var named = false;
-            var unnamed = false;
+            var invalidArgs = new List<string>();
+            var filter = (SearchLineage: false, TagGroups: new List<string>(), Start: "", Middle: new List<string>(), End: "");
             
-            var startFilter = "";
-            var endFilter = "";
-            var filter = "";
-
             while (args.Count >= 1)
             {
-                switch (args[0].ToLower())
-                {
-                    case "starts:":
-                    case "startswith:":
-                    case "starts_with:":
-                    case "starting:":
-                    case "startingwith:":
-                    case "starting_with:":
-                    case "start_filter:":
-                    case "starting_filter:":
-                        startFilter = args[1];
-                        args.RemoveAt(1);
-                        goto case "named";
-
-                    case "ends:":
-                    case "ending:":
-                    case "endingwith:":
-                    case "ending_with:":
-                    case "endswith:":
-                    case "ends_with:":
-                    case "end_filter:":
-                    case "ending_filter:":
-                        endFilter = args[1];
-                        args.RemoveAt(1);
-                        goto case "named";
-
-                    case "named:":
-                    case "filter:":
-                    case "contains:":
-                    case "containing:":
-                        filter = args[1];
-                        args.RemoveAt(1);
-                        goto case "named";
-
-                    case "named":
-                        named = true;
-                        break;
-
-                    case "unnamed":
-                        unnamed = true;
-						break;
-
-                    default:
-                        return new TagToolError(CommandError.ArgInvalid, $"\"{args[0]}\"");
-                }
-
+                var option = args[0].ToLower().Split(':');
                 args.RemoveAt(0);
+
+                // Is tag group
+                if (option.Length == 1)
+                {
+                    var tagGroup = Tag.Parse(Cache, option[0]);
+                    if (tagGroup.IsNull())
+                        invalidArgs.Add(option[0]);
+                    else
+                        filter.TagGroups.Add(tagGroup.ToString());
+                }
+                // Is option
+                else
+                {
+                    // Grab option value from next arg if needed
+                    if (option[1] == "")
+                    {
+                        if (args.Count == 0 || args[0].Contains(":"))
+                        {
+                            invalidArgs.Add(option[0]+":");
+                            continue;
+                        }
+                        else
+                        {
+                            option[1] = args[0].ToLower();
+                            args.RemoveAt(0);
+                        }
+                    }
+
+                    // Fill filter lists
+                    if (option[0].StartsWith("start") || option[0].StartsWith("begin"))
+                        filter.Start = option[1];
+                    else if (Array.IndexOf(new string[] { "filter", "name", "named", "contain", "contains", "containing", "has" }, option[0]) >= 0)
+                        filter.Middle.Add(option[1]);
+                    else if (option[0].StartsWith("end"))
+                        filter.End = option[1];
+                    else if (option[0] == "children" && option[1] == "true")
+                        filter.SearchLineage = true;
+                    else
+                        invalidArgs.Add(option[0] + ":" + option[1]);
+                }
             }
 
+            
+            // Warn of invalid tag groups/options
+            if (invalidArgs.Count > 0)
+                return new TagToolError(CommandError.ArgInvalid, $"\"{String.Join(", ", invalidArgs.ToArray())}\"");
+
+
+            // Print matches
             foreach (var tag in Cache.TagCache.TagTable)
             {
-                if (tag == null || tag.Group is null|| (groupTag != Tag.Null && !tag.IsInGroup(groupTag)))
-                    continue;
-                string groupName;
-                if (Cache.Version > CacheVersion.Halo3Beta) // gen 1 and gen 2 tag groups don't have a name stringid
-                    groupName = tag.Group.ToString();
-                else
-                    groupName = tag.Group.Tag.ToString();
-                
-                if (named)
+                if (
+                    tag is null ||
+                    tag.Name is null ||
+                    tag.Group is null ||
+                    !tag.Name.StartsWith(filter.Start) ||
+                    (filter.Middle.Count() > 0 && !filter.Middle.All(str => tag.Name.ToString().Contains(str))) ||
+                    !tag.Name.EndsWith(filter.End)
+                ) continue;
+
+                if (filter.TagGroups.Count > 0)
                 {
-                    if (tag.Name == null)
-                        continue;
-
-                    if (!tag.Name.StartsWith(startFilter) || !tag.Name.Contains(filter) || !tag.Name.EndsWith(endFilter))
-                        continue;
+                    if (filter.SearchLineage)
+                    {
+                        var groupLineage = new List<String> {tag.Group.Tag.ToString(), tag.Group.ParentTag.ToString(), tag.Group.GrandParentTag.ToString()};
+                        if (filter.TagGroups.Intersect(groupLineage).Count() == 0)
+                            continue;
+                    }
+                    else
+                    {
+                        if (!filter.TagGroups.Contains(tag.Group.Tag.ToString()))
+                            continue;
+                    }
                 }
-                
-                if (unnamed && tag.Name == null)
-                    Console.WriteLine($"[Index: 0x{tag.Index:X4}, Offset: 0x{tag.DefinitionOffset:X8}] {groupName} ({tag.Group.Tag})");
-                else if (named && tag.Name != null)
-                    Console.WriteLine($"[Index: 0x{tag.Index:X4}, Offset: 0x{tag.DefinitionOffset:X8}] {tag.Name}.{groupName}");
-                else if (!named && !unnamed)
-                    Console.WriteLine($"[Index: 0x{tag.Index:X4}, Offset: 0x{tag.DefinitionOffset:X8}] {tag.Name}.{groupName}");
-            }
 
-            return true;
+                if (tag.Name == "") tag.Name = "[UNNAMED]";
+
+                Console.WriteLine($"[Index: 0x{tag.Index:X4}, Offset: 0x{tag.DefinitionOffset:X8}] {tag.Name}.{tag.Group}");
+            }
+			
+			return true;
         }
     }
 }
