d76d8cb9014fcfe02e5ba18782257d5b91e8442d
Vegetal-Pole <vegetal.halo@gmail.com>
2021-10-16 17:21:05 -0500

    Update jpt! and ligh

--- TagTool/Tags/Definitions/Light.cs
+++ TagTool/Tags/Definitions/Light.cs
@@ -9,37 +9,21 @@ namespace TagTool.Tags.Definitions
     public class Light : TagStructure
 	{
         public LightFlags Flags;
-
-        [Flags]
-        public enum LightFlags : int
-        {
-            None = 0,
-            AllowShadowsAndGels = 1 << 0,
-            ShadowCasting = 1 << 1,
-            RenderFirstPersonOnly = 1 << 2,
-            RenderThirdPersonOnly = 1 << 3,
-            DontRenderSplitscreen = 1 << 4,
-            RenderWhileActiveCamo = 1 << 5,
-            RenderInMultiplayerOverride = 1 << 6,
-            MoveToCameraInFirstPerson = 1 << 7,
-            NeverPriorityCull = 1 << 8,
-            AffectedByGameCanUseFlashlights = 1 << 9,
-            ScreenspaceSpecularLighting = 1 << 10,
-            AlwaysOnForWeapon = 1 << 11
-        }
-
         public TypeValue Type;
-        public short Unknown;
-        public float SphereLightRange;
+
+        [TagField(Length = 2, Flags = TagFieldFlags.Padding)]
+        public byte[] Padding;
+
+        public float MaximumDistance; // maximum distance the light reaches (the light can become very dark well before this distance if you set your distance diffusion low) (world units)
 
         [TagField(MinVersion = CacheVersion.HaloReach)]
         public float UnknownIntensity;
         [TagField(MinVersion = CacheVersion.HaloReach)]
         public float Unknown0;
 
-        public float FrustumLightNearWidth;
-        public float FrustumLightHeightStretch;
-        public float FrustumLightFov;
+        public float FrustumNearWidth; // width of the frustum at the near plane (world units)
+        public float FrustumHeightScale; // how much the gel is stretched vertically (0.0 or 1.0 = aspect ratio same as gel)
+        public float FrustumFieldOfView; // horizontal angle that the frustum light covers (0.0 = no spread, a straight beam of light)
 
         [TagField(MinVersion = CacheVersion.HaloReach)]
         public float UnknownAngle;
@@ -56,39 +40,24 @@ namespace TagTool.Tags.Definitions
         [TagField(MinVersion = CacheVersion.HaloReach)]
         public short Unknown4;
 
-        public enum ModifierEnum : short
-        {
-            None,
-            Plus,
-            Times
-        }
-
-        public StringId ColorChangeInputVariable;
-        public StringId ColorChangeRangeVariable;
-        public ModifierEnum ColorChangeOutputModifier;
-        public short Unknown6;
-        public StringId CcOutputModifierInput;
-        public TagFunction ColorChangeFunction = new TagFunction { Data = new byte[0] };
-        public StringId IntensityInputVariable;
-        public StringId IntensityRangeVariable;
-        public ModifierEnum IntensityOutputModifier;
-        public short Unknown9;
-        public StringId IntesityOutputModifierInput;
-        public TagFunction IntensityFunction = new TagFunction { Data = new byte[0] };
-        public CachedTag GelMap;
+        public LightColorFunctionStruct Color;
+        public LightScalarFunctionStruct Intensity;
+        public CachedTag GelBitmap; // requires 'expensive light'
 
         [TagField(MaxVersion = CacheVersion.HaloOnline700123)]
-        public float FrustumMinimumViewDistance;
+        public float DistanceDiffusion; // [0.01 - 10.0+] approximately the physical size in world units of the light source itself.  small values cause the light to become very bright close to the light source, but quickly die off as you move away
         [TagField(MaxVersion = CacheVersion.HaloOnline700123)]
-        public float MaxIntensityRange;
+        public float AngularSmoothness; // [0.2 - 8.0] less than 1.0 for sharp edges, greater than 1.0 for smooth edges
 
-        public float Unknown12;
-        public float Unknown13;
+        public float PercentSpherical; // [0.0 - 1.0] percentage of ambient (spherical) light
+        public float DestroyLightAfter; // automatically destroys the light after this many seconds (0 to disable)
+        public LightPriorityEnumeration NearPriority; // priority when the light is fullscreen in size
+        public LightPriorityEnumeration FarPriority; // priority when the light is very far away
+        public LightPriorityBiasEnumeration TransitionDistance; // specifies where the transition occurs between near and far priority
 
-        public sbyte Unknown14;
-        public sbyte Unknown15;
-        public sbyte Unknown16;
-        public sbyte Unknown17;
+        [TagField(Length = 1, Flags = TagFieldFlags.Padding)]
+        public byte[] Padding1;
+
         public CachedTag LensFlare;
 
         [TagField(MinVersion = CacheVersion.HaloReach)]
@@ -109,10 +78,95 @@ namespace TagTool.Tags.Definitions
         [TagField(MinVersion = CacheVersion.HaloReach)]
         public float Unknown22;
 
+        [Flags]
+        public enum LightFlags : int
+        {
+            None = 0,
+            AllowShadowsAndGels = 1 << 0, // CPU lights can have shadows and gels
+            ShadowCasting = 1 << 1, // turns on shadow casting
+            RenderFirstPersonOnly = 1 << 2, // only render when camera is 1st person
+            RenderThirdPersonOnly = 1 << 3, // don't render when camera is 1st person
+            DontRenderSplitscreen = 1 << 4, // no rendering this light in splitscreen mode
+            RenderWhileActiveCamo = 1 << 5, // keep rendering this light when the attached player goes camo
+            RenderInMultiplayerOverride = 1 << 6, // overrides game settings that disable dynamic lights
+            MoveToCameraInFirstPerson = 1 << 7, // moves the light to match the camera
+            NeverPriorityCull = 1 << 8, // never cull this light because of low priority
+            AffectedByGameCanUseFlashlights = 1 << 9,
+            ScreenspaceSpecularLighting = 1 << 10,
+            AlwaysOnForWeapon = 1 << 11
+        }
+
         public enum TypeValue : short
         {
             Sphere,
-            Projective,
+            Frustum,
+        }
+
+        public enum ModifierEnum : short
+        {
+            None,
+            Plus,
+            Times
+        }
+
+        [TagStructure(Size = 0x24)]
+        public class LightColorFunctionStruct : TagStructure
+        {
+            public StringId InputVariable;
+            public StringId RangeVariable;
+            public ModifierEnum OutputModifier;
+
+            [TagField(Length = 2, Flags = TagFieldFlags.Padding)]
+            public byte[] Padding;
+
+            public StringId OutputModifierInput;
+            public TagFunction Function = new TagFunction { Data = new byte[0] };
+        }
+
+        [TagStructure(Size = 0x24)]
+        public class LightScalarFunctionStruct : TagStructure
+        {
+            public StringId InputVariable;
+            public StringId RangeVariable;
+            public ModifierEnum OutputModifier;
+
+            [TagField(Length = 2, Flags = TagFieldFlags.Padding)]
+            public byte[] Padding;
+
+            public StringId OutputModifierInput;
+            public TagFunction Function = new TagFunction { Data = new byte[0] };
+        }
+
+        public enum LightTypeEnumDefinition : short
+        {
+            Sphere,
+            Frustum
+        }
+
+        public enum LightPriorityEnumeration : sbyte
+        {
+            Default,
+            InsanelyHigh,
+            _1VeryHigh,
+            _2,
+            _3High,
+            _4,
+            _5Default,
+            _6,
+            _7Low,
+            _8,
+            _9VeryLow,
+            NextToNothing
+        }
+
+        public enum LightPriorityBiasEnumeration : sbyte
+        {
+            Default,
+            VeryClose,
+            Close,
+            Middle,
+            Far,
+            VeryFar
         }
     }
 }
