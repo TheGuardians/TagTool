6231b114a1654298a7fba619fae5efa6f2e98755
Vegetal-Pole <vegetal.halo@gmail.com>
2021-11-21 14:34:32 -0600

    Update pmov

---
+++ TagTool/Audio/SoundPermutationLanguagesBlock.cs
@@ -0,0 +1,69 @@
+﻿using TagTool.Tags;
+using TagTool.Cache;
+
+namespace TagTool.Audio
+{
+    [TagStructure(Size = 0x14, MinVersion = CacheVersion.Halo3Beta, Platform = CachePlatform.Original)]
+    [TagStructure(Size = 0x1C, MinVersion = CacheVersion.Halo3Retail, Platform = CachePlatform.MCC)]
+    public class SoundPermutationLanguagesBlock : TagStructure
+	{
+        public int Offset;
+
+        // Encoded size, to get the real size apply a mask of 0x3FFFFF.
+        // It should always have the bit 0x400000 activated.
+        public int EncodedSizeAndFlags;
+        public int RuntimeIndex; // Changes at runtime depending on the g_xbox_sound data array index
+        public uint FirstSample;
+        public uint LastSample;
+
+        [TagField(Platform = CachePlatform.MCC)]
+        public uint RuntimeFsbSoundHash;
+        [TagField(Platform = CachePlatform.MCC)]
+        public int RuntimeFsbSoundRuntimeStatus;
+
+        public SoundPermutationLanguagesBlock()
+        {
+            Offset = 0;
+            EncodedSizeAndFlags = 0x4000000;
+            RuntimeIndex = -1;
+            LastSample = 0;
+            FirstSample = 0;
+        }
+
+        public SoundPermutationLanguagesBlock(int offset, int size)
+        {
+            Offset = offset;
+            EncodedSizeAndFlags = (0x3FFFFFF & size) + 0x4000000;
+            RuntimeIndex = -1;
+            LastSample = 0;
+            FirstSample = 0;
+        }
+        
+        public SoundPermutationLanguagesBlock(int offset, int size, uint lastSample, uint firstSample)
+        {
+            Offset = offset;
+            EncodedSizeAndFlags = (0x3FFFFFF & size) + 0x4000000;
+            RuntimeIndex = -1;
+            LastSample = lastSample;
+            FirstSample = firstSample;
+        }
+    }
+
+    [TagStructure(Size = 0xC)]
+    public class Gen2PermutationChunk : TagStructure
+    {
+        public TagResourceReference ResourceReference;
+
+        /// <summary>
+        /// Encoded size, to get the real size apply a mask of 0x3FFFFF.
+        /// </summary>
+        public int EncodedSize;
+
+        public int RuntimeIndex;
+
+        public int GetSize()
+        {
+            return EncodedSize & 0x3FFFFFF;
+        }
+    }
+}
---
+++ TagTool/Audio/SoundPromotionStruct.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using TagTool.Cache;
+using TagTool.Common;
+using TagTool.Tags;
+
+namespace TagTool.Audio
+{
+    [TagStructure(Size = 0x1C, MinVersion = CacheVersion.Halo2Beta, MaxVersion = CacheVersion.Halo2Vista)]
+    [TagStructure(Size = 0x24, Platform = CachePlatform.Original, MinVersion = CacheVersion.Halo3Retail, MaxVersion = CacheVersion.Halo3ODST)]
+    [TagStructure(Size = 0x30, MinVersion = CacheVersion.HaloOnlineED, MaxVersion = CacheVersion.HaloOnline700123)]
+    [TagStructure(Size = 0x24, MinVersion = CacheVersion.HaloReach)]
+    [TagStructure(Size = 0x30, Platform = CachePlatform.MCC, MinVersion = CacheVersion.Halo3Retail)]
+    public class SoundPromotionStruct : TagStructure
+    {
+        public List<PromotionRule> Rules;
+        public List<PromotionRuntimeTimer> RuntimeTimers;
+
+        public uint RuntimeActivePromotionIndex;
+        public uint RuntimeLastPromotionTime;
+        public uint RuntimeSuppressionTimeout;
+
+        [TagField(Gen = CacheGeneration.HaloOnline)]
+        public uint LongestPermutationDuration;
+        [TagField(Gen = CacheGeneration.HaloOnline)]
+        public uint TotalSampleSize;
+
+        [TagField(Length = 8, Flags = TagFieldFlags.Padding, Platform = CachePlatform.MCC, MinVersion = CacheVersion.Halo3Retail)]
+        public byte[] PaddingMCC;
+
+        [TagField(Gen = CacheGeneration.HaloOnline)]
+        [TagField(Platform = CachePlatform.MCC, MinVersion = CacheVersion.Halo3Retail)]
+        public StringId RuntimeUsage;
+
+        [TagStructure(Size = 0x10)]
+        public class PromotionRule : TagStructure
+		{
+            public short PitchRangeIndex;
+            public short MaximumPlayingCount;
+            public float SuppressionTime; // (seconds) time from when first permutation plays to when another sound from an equal or lower promotion can play
+            public uint RuntimeRolloverTime;
+            public uint ImpulsePromotionTime;
+        }
+
+        [TagStructure(Size = 0x4)]
+        public class PromotionRuntimeTimer : TagStructure
+		{
+            public int TimerStorage;
+        }
+    }
+}
