6bbd4b9752c79027ce5370ad93736310bfca0a19
unk-1 <unk0x4@gmail.com>
2021-12-19 22:40:03 +0000

    Reach decal system definition

--- TagTool/Tags/Definitions/DecalSystem.cs
+++ TagTool/Tags/Definitions/DecalSystem.cs
@@ -1,16 +1,30 @@
 using TagTool.Common;
 using System.Collections.Generic;
 using System;
+using TagTool.Cache;
 
 namespace TagTool.Tags.Definitions
 {
-    [TagStructure(Name = "decal_system", Tag = "decs", Size = 0x24)]
+    [TagStructure(Name = "decal_system", Tag = "decs", Size = 0x24, MaxVersion = CacheVersion.HaloOnline700123)]
+    [TagStructure(Name = "decal_system", Tag = "decs", Size = 0x3C, MinVersion = CacheVersion.HaloReach)]
     public class DecalSystem : TagStructure
 	{
         public FlagsValue Flags;
         public int MaxOverlapping; // 0= no limit
+        [TagField(MaxVersion = CacheVersion.HaloOnline700123)]
         public float OverlappingThreshold;
+
+        [TagField(MinVersion = CacheVersion.HaloReach)]
+        public float MaterialShaderFadeTime;
+        [TagField(MinVersion = CacheVersion.HaloReach)]
+        public int Unknown1; // flags for the tag below?
+        [TagField(MinVersion = CacheVersion.HaloReach)]
+        public float Unknown2; // delay for the tag below?
+        [TagField(MinVersion = CacheVersion.HaloReach)]
+        public CachedTag Unknown3;
+
         public Bounds<float> DistanceFadeRange;
+
         public List<DecalDefinitionBlock> Decal;
         public float RuntimeMaxRadius;
 
@@ -28,13 +42,17 @@ namespace TagTool.Tags.Definitions
             DontCollideWithInstances = 1 << 8
         }
 
-        [TagStructure(Size = 0x74)]
+        [TagStructure(Size = 0x74, MaxVersion = CacheVersion.HaloOnline700123)]
+        [TagStructure(Size = 0x98, MinVersion = CacheVersion.HaloReach)]
         public class DecalDefinitionBlock : TagStructure
 		{
             public StringId DecalName;
             public FlagsValue Flags;
 
             public RenderMethod RenderMethod;
+
+            [TagField(MinVersion = CacheVersion.HaloReach)]
+            public uint Unknown1;
 
             [TagField(Format = "World Units")]
             public Bounds<float> Radius;
@@ -56,6 +74,7 @@ namespace TagTool.Tags.Definitions
             public float CullAngle;
 
             public DecalPass Pass;
+            [TagField(MaxVersion = CacheVersion.HaloOnline700123)]
             public float SpecularMultiplier;
             public float RuntimeBitmapAspect;
 
