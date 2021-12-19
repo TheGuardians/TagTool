6c802bfe2d489bb6fa1fb4b60c1a6267c721297f
unk-1 <unk0x4@gmail.com>
2021-12-19 22:09:19 +0000

    Update decs

--- TagTool/Tags/Definitions/DecalSystem.cs
+++ TagTool/Tags/Definitions/DecalSystem.cs
@@ -8,69 +8,24 @@ namespace TagTool.Tags.Definitions
     public class DecalSystem : TagStructure
 	{
         public FlagsValue Flags;
-
-        /// <summary>
-        /// Bigger sizes keep more decals around but use much more memory
-        /// </summary>
-        [TagField(Format = "Triangles")]
-        public short RingBufferSize;
-
-        /// <summary>
-        /// Above entry is for MP -- SP can be different
-        /// </summary>
-        [TagField(Format = "Triangles")]
-        public short RingBufferSizeSinglePlayer;
-
-        /// <summary>
-        /// Material shader lifetime is modulated from 1 to 0 over this time
-        /// </summary>
-        public float MaterialShaderFadeTime;
-
-        /// <summary>
-        /// If set to non-zero, this will override manual scaling in Sapien and smash it with these values
-        /// </summary>
-        public RealPoint2d DecalScaleOverride;
-
+        public int MaxOverlapping; // 0= no limit
+        public float OverlappingThreshold;
+        public Bounds<float> DistanceFadeRange;
         public List<DecalDefinitionBlock> Decal;
-
         public float RuntimeMaxRadius;
 
         [Flags]
         public enum FlagsValue : int
         {
-            None = 0,
-            Bit0 = 1 << 0,
-            Bit1 = 1 << 1,
-            Bit2 = 1 << 2,
-            Bit3 = 1 << 3,
-            Bit4 = 1 << 4,
-            Bit5 = 1 << 5,
-            Bit6 = 1 << 6,
-            Bit7 = 1 << 7,
-            Bit8 = 1 << 8,
-            Bit9 = 1 << 9,
-            Bit10 = 1 << 10,
-            Bit11 = 1 << 11,
-            Bit12 = 1 << 12,
-            Bit13 = 1 << 13,
-            Bit14 = 1 << 14,
-            Bit15 = 1 << 15,
-            Bit16 = 1 << 16,
-            Bit17 = 1 << 17,
-            Bit18 = 1 << 18,
-            Bit19 = 1 << 19,
-            Bit20 = 1 << 20,
-            Bit21 = 1 << 21,
-            Bit22 = 1 << 22,
-            Bit23 = 1 << 23,
-            Bit24 = 1 << 24,
-            Bit25 = 1 << 25,
-            Bit26 = 1 << 26,
-            Bit27 = 1 << 27,
-            Bit28 = 1 << 28,
-            Bit29 = 1 << 29,
-            Bit30 = 1 << 30,
-            Bit31 = 1 << 31
+            RandomRotation = 1 << 0,
+            RandomUMirror = 1 << 1,
+            RandomVMirror = 1 << 2,
+            ForceQuaduseWithCare = 1 << 3,
+            ForcePlanar = 1 << 4,
+            RestrictToSingleMaterial = 1 << 5,
+            UsePrimaryCollisionOnly = 1 << 6,
+            DontCollideWithStructure = 1 << 7,
+            DontCollideWithInstances = 1 << 8
         }
 
         [TagStructure(Size = 0x74)]
