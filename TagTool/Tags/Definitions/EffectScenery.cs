47079c4a2ac20ed73ab3d98607c172d8e748a3eb
unk-1 <unk0x4@gmail.com>
2022-01-16 15:39:56 +0000

    Fix size warnings for reach

--- TagTool/Tags/Definitions/EffectScenery.cs
+++ TagTool/Tags/Definitions/EffectScenery.cs
@@ -4,10 +4,11 @@ using static TagTool.Tags.TagFieldFlags;
 namespace TagTool.Tags.Definitions
 {
     [TagStructure(Name = "effect_scenery", Tag = "efsc", Size = 0x0, MaxVersion = CacheVersion.Halo3ODST)]
-    [TagStructure(Name = "effect_scenery", Tag = "efsc", Size = 0xC, MinVersion = CacheVersion.HaloOnlineED)]
+    [TagStructure(Name = "effect_scenery", Tag = "efsc", Size = 0xC, MinVersion = CacheVersion.HaloOnlineED, MaxVersion = CacheVersion.HaloOnline700123)]
+    [TagStructure(Name = "effect_scenery", Tag = "efsc", Size = 0x0, MinVersion = CacheVersion.HaloReach)]
     public class EffectScenery : GameObject
     {
-        [TagField(Flags = Padding, Length = 12, MinVersion = CacheVersion.HaloOnlineED)]
+        [TagField(Flags = Padding, Length = 12, MinVersion = CacheVersion.HaloOnlineED, MaxVersion = CacheVersion.HaloOnline700123)]
         public byte[] Unused4;
     }
 }
