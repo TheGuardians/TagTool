3e75545059e8bedb31f88f4c15bccdeaa5cf2118
Hariiiiiiii <46051616+Hariiiiiiii@users.noreply.github.com>
2022-05-18 19:17:25 -0400

    more gen2 bsp porting work

--- TagTool/Commands/Porting/Gen2/PortTagGen2Command.Scenarios.cs
+++ TagTool/Commands/Porting/Gen2/PortTagGen2Command.Scenarios.cs
@@ -9,6 +9,8 @@ using TagTool.Common;
 using TagTool.Geometry;
 using static TagTool.Commands.Porting.Gen2.Gen2RenderGeometryConverter;
 using TagTool.Commands.Common;
+using TagTool.Havok;
+using TagTool.Cache;
 
 namespace TagTool.Commands.Porting.Gen2
 {
@@ -42,16 +44,65 @@ namespace TagTool.Commands.Porting.Gen2
             foreach (var collisiongeo in gen2Tag.CollisionBsp)
                 CollisionResource.CollisionBsps.Add(collisiongeo);
 
-            //cluster data
-            foreach(var cluster in gen2Tag.ClusterData)
+            //structure physics
+            newSbsp.Physics = new ScenarioStructureBsp.StructurePhysics
             {
+                MoppBoundsMin = gen2Tag.StructurePhysics.MoppBoundsMin,
+                MoppBoundsMax = gen2Tag.StructurePhysics.MoppBoundsMax
+            };
+            var moppCode = HavokMoppGenerator.GenerateMoppCode(gen2Tag.CollisionBsp[0]);
+            if (moppCode == null)
+                new TagToolError(CommandError.OperationFailed, "Failed to generate mopp code!");
+            moppCode.Data.AddressType = CacheAddressType.Data;
+            newSbsp.Physics.CollisionMoppCodes = new List<TagHkpMoppCode>();
+            newSbsp.Physics.CollisionMoppCodes.Add(moppCode);
 
+            //cluster data
+            foreach (var cluster in gen2Tag.Clusters)
+            {
+                //render geometry
+                var compressor = new VertexCompressor(
+                    cluster.SectionInfo.Compression.Count > 0 ?
+                        cluster.SectionInfo.Compression[0] :
+                        new RenderGeometryCompression
+                        {
+                            X = new Bounds<float>(0.0f, 1.0f),
+                            Y = new Bounds<float>(0.0f, 1.0f),
+                            Z = new Bounds<float>(0.0f, 1.0f),
+                            U = new Bounds<float>(0.0f, 1.0f),
+                            V = new Bounds<float>(0.0f, 1.0f),
+                            U2 = new Bounds<float>(0.0f, 1.0f),
+                            V2 = new Bounds<float>(0.0f, 1.0f),
+                        });
+                List<Gen2ResourceMesh> clustermeshes = ReadResourceMeshes(Gen2Cache, cluster.GeometryBlockInfo,
+                    cluster.SectionInfo.TotalVertexCount, (RenderGeometryCompressionFlags)cluster.SectionInfo.GeometryCompressionFlags,
+                    (TagTool.Tags.Definitions.Gen2.RenderModel.SectionLightingFlags)cluster.SectionInfo.SectionLightingFlags, compressor);
+
+                if (clustermeshes.Count > 1)
+                {
+                    new TagToolWarning("cluster had >1 render mesh! Culling extras...");
+                    clustermeshes = new List<Gen2ResourceMesh> { clustermeshes.First() };
+                }
+
+                BuildMeshes(builder, clustermeshes, (RenderGeometryClassification)cluster.SectionInfo.GeometryClassification,
+                    cluster.SectionInfo.OpaqueMaxNodesVertex, 0);
+
+                //block values
+                var newcluster = new ScenarioStructureBsp.Cluster
+                {
+                    //mesh that was just built
+                    MeshIndex = (short)(builder.Meshes.Count - 1),
+                    BoundsX = cluster.BoundsX,
+                    BoundsY = cluster.BoundsY,
+                    BoundsZ = cluster.BoundsZ
+                };
             }
 
             //instanced geometry definitions
             CollisionResource.InstancedGeometry = new TagBlock<InstancedGeometryBlock>();
             foreach(var instanced in gen2Tag.InstancedGeometriesDefinitions)
             {
+                //render geometry
                 var compressor = new VertexCompressor(
                     instanced.RenderInfo.SectionInfo.Compression.Count > 0 ?
                         instanced.RenderInfo.SectionInfo.Compression[0] :
@@ -71,14 +122,15 @@ namespace TagTool.Commands.Porting.Gen2
                 
                 if(instancemeshes.Count > 1)
                 {
-                    new TagToolWarning("instance had >1 mesh! Culling extras...");
+                    new TagToolWarning("instance had >1 render mesh! Culling extras...");
                     instancemeshes = new List<Gen2ResourceMesh> { instancemeshes.First() };
                 }
                 
                 BuildMeshes(builder, instancemeshes, (RenderGeometryClassification)instanced.RenderInfo.SectionInfo.GeometryClassification, 
                     instanced.RenderInfo.SectionInfo.OpaqueMaxNodesVertex, 0);
 
-                CollisionResource.InstancedGeometry.Add(new InstancedGeometryBlock
+                //block values
+                var newinstance = new InstancedGeometryBlock
                 {
                     Checksum = instanced.Checksum,
                     BoundingSphereOffset = instanced.BoundingSphereCenter,
@@ -86,27 +138,61 @@ namespace TagTool.Commands.Porting.Gen2
                     CollisionInfo = instanced.CollisionInfo,
                     //index of mesh just built
                     MeshIndex = (short)(builder.Meshes.Count - 1),
-                });
+                };
+
+                //build mopp codes from collision info and add
+                if (instanced.BspPhysics != null && instanced.BspPhysics.Count > 0)
+                {
+                    var mopp = HavokMoppGenerator.GenerateMoppCode(newinstance.CollisionInfo);
+                    if (mopp == null)
+                        new TagToolError(CommandError.OperationFailed, "Failed to generate mopp code!");
+                    mopp.Data.AddressType = CacheAddressType.Data;
+                    newinstance.CollisionMoppCodes = new TagBlock<TagHkpMoppCode>(CacheAddressType.Definition);
+                    newinstance.CollisionMoppCodes.Add(mopp);
+                }
+
+                CollisionResource.InstancedGeometry.Add(newinstance);
             }
 
             //instanced geometry instances
             newSbsp.InstancedGeometryInstances = new List<InstancedGeometryInstance>();
             foreach (var instanced in gen2Tag.InstancedGeometryInstances)
             {
-                //TODO: BSP Physics, Matrix
-                newSbsp.InstancedGeometryInstances.Add(new InstancedGeometryInstance
+                var newinstance = new InstancedGeometryInstance
                 {
                     Scale = instanced.Scale,
-                    Matrix = new RealMatrix4x3 { },
+                    Matrix = new RealMatrix4x3(instanced.Forward.I, instanced.Forward.J, instanced.Forward.K,
+                    instanced.Left.I, instanced.Left.J, instanced.Left.K,
+                    instanced.Up.I, instanced.Up.J, instanced.Up.K,
+                    instanced.Position.X, instanced.Position.Y, instanced.Position.Z),
                     DefinitionIndex = instanced.InstanceDefinition,
                     Flags = (InstancedGeometryInstance.FlagsValue)instanced.Flags,
                     Name = instanced.Name,
                     WorldBoundingSphereCenter = instanced.WorldBoundingSphereCenter,
                     BoundingSphereRadiusBounds = new Bounds<float>(0, instanced.BoundingSphereRadius),
                     PathfindingPolicy = (Scenery.PathfindingPolicyValue)instanced.PathfindingPolicy,
-                    LightmappingPolicy = (InstancedGeometryInstance.InstancedGeometryLightmappingPolicy)instanced.LightmappingPolicy
+                    LightmappingPolicy = (InstancedGeometryInstance.InstancedGeometryLightmappingPolicy)instanced.LightmappingPolicy,
+                };
 
-                });
+                //make sure there is a bsp physics block in the instance def
+                var instancedef = gen2Tag.InstancedGeometriesDefinitions[instanced.InstanceDefinition];
+                if(instancedef.BspPhysics != null && instancedef.BspPhysics.Count > 0)
+                {
+                    newinstance.BspPhysics = new List<CollisionBspPhysicsDefinition>
+                    {
+                        new CollisionBspPhysicsDefinition
+                        {
+                            MoppBvTreeShape = new CMoppBvTreeShape(),
+                            GeometryShape = new CollisionGeometryShape
+                            {
+                                AABB_Center = instancedef.BspPhysics[0].AABB_Center,
+                                AABB_Half_Extents = instancedef.BspPhysics[0].AABB_Half_Extents
+                            }
+                        }
+                    };
+                }
+
+                newSbsp.InstancedGeometryInstances.Add(newinstance);
             }
 
             //close out render geo resource and prepare to load it in to the lightmap
