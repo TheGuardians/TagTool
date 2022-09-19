d011e2bc004a8724e82676c9f22a6c64f441ec2c
Hariiiiiiii <46051616+Hariiiiiiii@users.noreply.github.com>
2022-09-19 17:50:12 -0400

    Update ExportJMSCommand.cs

--- TagTool/Commands/Models/ExportJMSCommand.cs
+++ TagTool/Commands/Models/ExportJMSCommand.cs
@@ -63,39 +63,25 @@ namespace TagTool.Commands.Models
                 file.Directory.Create();
 
             JmsFormat jms = new JmsFormat();
-
-            //build nodes
-            foreach (var node in Definition.Nodes)
-            {
-                var newnode = new JmsFormat.JmsNode
-                {
-                    Name = Cache.StringTable.GetString(node.Name),
-                    ParentNodeIndex = node.ParentNode,
-                    Rotation = node.DefaultRotation,
-                    Position = new RealVector3d(node.DefaultTranslation.X, node.DefaultTranslation.Y, node.DefaultTranslation.Z) * 100.0f
-                };
-                if (!newnode.Name.StartsWith("b_"))
-                    newnode.Name = "b_" + newnode.Name;
-                if (newnode.ParentNodeIndex != -1)
-                {
-                    Matrix4x4 transform = MatrixFromNode(newnode.Rotation, newnode.Position);
-                    Matrix4x4 parent_transform = MatrixFromNode(jms.Nodes[newnode.ParentNodeIndex].Rotation,
-                        jms.Nodes[newnode.ParentNodeIndex].Position);
-                    Matrix4x4 result = Matrix4x4.Multiply(transform, parent_transform);
-
-                    Vector3 out_scale = new Vector3();
-                    Vector3 out_translate = new Vector3();
-                    Quaternion out_rotate = new Quaternion();
-                    Matrix4x4.Decompose(result, out out_scale, out out_rotate, out out_translate);
-                    newnode.Position = new RealVector3d(out_translate.X * out_scale.X, out_translate.Y * out_scale.Y, out_translate.Z * out_scale.Z);
-                    newnode.Rotation = new RealQuaternion(out_rotate.X, out_rotate.Y, out_rotate.Z, out_rotate.W);
-                }
-
-                jms.Nodes.Add(newnode);
-            }
-
+            
             using (var cacheStream = Cache.OpenCacheRead())
             {
+                //build nodes
+                if (Definition.Nodes != null)
+                {
+                    BuildNodesFromHlmt(jms, Definition);
+                }
+                else if (Definition.RenderModel != null)
+                {
+                    RenderModel mode = Cache.Deserialize<RenderModel>(cacheStream, Definition.RenderModel);
+                    BuildNodesFromMode(jms, mode);
+                }
+                else
+                {
+                    return new TagToolError(CommandError.OperationFailed, "Model has no nodes, couldn't build JMS!");
+                }
+
+                //add content
                 if (ExportCollision && Definition.CollisionModel != null)
                 {
                     CollisionModel coll = Cache.Deserialize<CollisionModel>(cacheStream, Definition.CollisionModel);
@@ -129,6 +115,70 @@ namespace TagTool.Commands.Models
             rot.Translation = new Vector3(position.I, position.J, position.K);
 
             return rot;
+        }
+
+        public void BuildNodesFromMode(JmsFormat jms, RenderModel mode)
+        {
+            foreach (var node in mode.Nodes)
+            {
+                var newnode = new JmsFormat.JmsNode
+                {
+                    Name = Cache.StringTable.GetString(node.Name),
+                    ParentNodeIndex = node.ParentNode,
+                    Rotation = node.DefaultRotation,
+                    Position = new RealVector3d(node.DefaultTranslation.X, node.DefaultTranslation.Y, node.DefaultTranslation.Z) * 100.0f
+                };
+                if (!newnode.Name.StartsWith("b_"))
+                    newnode.Name = "b_" + newnode.Name;
+                if (newnode.ParentNodeIndex != -1)
+                {
+                    Matrix4x4 transform = MatrixFromNode(newnode.Rotation, newnode.Position);
+                    Matrix4x4 parent_transform = MatrixFromNode(jms.Nodes[newnode.ParentNodeIndex].Rotation,
+                        jms.Nodes[newnode.ParentNodeIndex].Position);
+                    Matrix4x4 result = Matrix4x4.Multiply(transform, parent_transform);
+
+                    Vector3 out_scale = new Vector3();
+                    Vector3 out_translate = new Vector3();
+                    Quaternion out_rotate = new Quaternion();
+                    Matrix4x4.Decompose(result, out out_scale, out out_rotate, out out_translate);
+                    newnode.Position = new RealVector3d(out_translate.X * out_scale.X, out_translate.Y * out_scale.Y, out_translate.Z * out_scale.Z);
+                    newnode.Rotation = new RealQuaternion(out_rotate.X, out_rotate.Y, out_rotate.Z, out_rotate.W);
+                }
+
+                jms.Nodes.Add(newnode);
+            }
+        }
+
+        public void BuildNodesFromHlmt(JmsFormat jms, Model hlmt)
+        {
+            foreach (var node in hlmt.Nodes)
+            {
+                var newnode = new JmsFormat.JmsNode
+                {
+                    Name = Cache.StringTable.GetString(node.Name),
+                    ParentNodeIndex = node.ParentNode,
+                    Rotation = node.DefaultRotation,
+                    Position = new RealVector3d(node.DefaultTranslation.X, node.DefaultTranslation.Y, node.DefaultTranslation.Z) * 100.0f
+                };
+                if (!newnode.Name.StartsWith("b_"))
+                    newnode.Name = "b_" + newnode.Name;
+                if (newnode.ParentNodeIndex != -1)
+                {
+                    Matrix4x4 transform = MatrixFromNode(newnode.Rotation, newnode.Position);
+                    Matrix4x4 parent_transform = MatrixFromNode(jms.Nodes[newnode.ParentNodeIndex].Rotation,
+                        jms.Nodes[newnode.ParentNodeIndex].Position);
+                    Matrix4x4 result = Matrix4x4.Multiply(transform, parent_transform);
+
+                    Vector3 out_scale = new Vector3();
+                    Vector3 out_translate = new Vector3();
+                    Quaternion out_rotate = new Quaternion();
+                    Matrix4x4.Decompose(result, out out_scale, out out_rotate, out out_translate);
+                    newnode.Position = new RealVector3d(out_translate.X * out_scale.X, out_translate.Y * out_scale.Y, out_translate.Z * out_scale.Z);
+                    newnode.Rotation = new RealQuaternion(out_rotate.X, out_rotate.Y, out_rotate.Z, out_rotate.W);
+                }
+
+                jms.Nodes.Add(newnode);
+            }
         }
     }
 }
