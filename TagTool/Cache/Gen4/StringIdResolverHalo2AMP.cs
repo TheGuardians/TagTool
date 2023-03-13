cd8410b638157770ec5ff8d35b100531b523dd60
Faber <103538724+FaberTheCatboy@users.noreply.github.com>
2023-03-13 16:49:27 -0700

    Correct Groundhog by renaming to Halo2AMP StringID Resolver

--- TagTool/Cache/Gen4/StringIdResolverHalo2AMP.cs
+++ TagTool/Cache/Gen4/StringIdResolverHalo2AMP.cs
@@ -3,16 +3,16 @@ using TagTool.Common;
 namespace TagTool.Cache
 {
     /// <summary>
-    /// StringID resolver for Groundhog. 
+    /// StringID resolver for Halo2AMP. 
     /// Halo 4 StringId are 5-8-19 (length-set-index) format instead of (8-8-16)
     /// </summary>
-    public class StringIdResolverGroundhog : StringIdResolver
+    public class StringIdResolverHalo2AMP : StringIdResolver
     {
         private static readonly int[] SetOffsets = { 0x210C, 0x633, 0xEC9, 0xFD4, 0x1061, 0x1159, 0x11A4, 0x11CF, 0x1CD7, 0x1EE7, 0x1F1B, 0x1F9E, 0x1FD2, 0x1FDF, 0x2021 };
         private const int SetMin = 0x633;   // Mininum index that goes in a set
         private const int SetMax = 0x7FFFF; // Maximum index that goes in a set
 
-        public StringIdResolverGroundhog()
+        public StringIdResolverHalo2AMP()
         {
             LengthBits = 5;
             SetBits = 8;
