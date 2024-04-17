2fb6525e344731fa1c5a1712d4cb6e3b113e385f
Modulus32 <46051616+Modulus32@users.noreply.github.com>
2024-04-17 15:40:34 -0400

    Update FMODSoundIndex.cs

--- TagTool/Audio/FMOD/FMODSoundIndex.cs
+++ TagTool/Audio/FMOD/FMODSoundIndex.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using TagTool.Cache;
+using TagTool.Commands.Common;
 using TagTool.IO;
 using TagTool.Serialization;
 using TagTool.Tags;
@@ -46,6 +47,12 @@ namespace TagTool.Audio
             while (!reader.EOF)
             {
                 var info = deserializer.Deserialize<FMODSoundInfo>(dataContext);
+                if(HashLookup.ContainsKey(info.Hash))
+                {
+                    new TagToolWarning($"Duplicate sound of {info.Filename} found in sound cache! Skipping...");
+                    index++;
+                    continue;
+                }
                 HashLookup.Add(info.Hash, index);
                 Sounds.Add(info);
                 index++;
